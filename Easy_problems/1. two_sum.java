3bbf9d5ba0410128048cc3d2dbf17821100287be
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-08-22 20:59:02 +0530

    Added Two Sum solution

--- Easy_problems/1. two_sum.java
+++ Easy_problems/1. two_sum.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution(); 
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] result = solution.twoSum(nums, target); 
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
+}
 
