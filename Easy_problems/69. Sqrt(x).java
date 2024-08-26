aa7f9ab31fdd7546a4c99f018bed55f130b24942
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-08-26 10:43:25 +0530

    added solution

--- Easy_problems/69. Sqrt(x).java
+++ Easy_problems/69. Sqrt(x).java
@@ -1,1 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low=0;
+        int high = x;
+           if(x<=1) return x;
+            while (low <= high) {
+                int mid = low + (high - low) / 2; // To avoid overflow
 
+                if (mid == x/mid) {
+                   return mid;
+                } else if (mid < x/mid) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            return high;//It is concept to understand why we have returned high
+    }
+}
+// Or we can also use long instead of int as long as big value than int
