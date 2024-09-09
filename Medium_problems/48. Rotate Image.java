3aed85a73e77560576d47f775c147a688d3cd03a
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-09-09 22:39:17 +0530

    Code Added

--- Medium_problems/48. Rotate Image.java
+++ Medium_problems/48. Rotate Image.java
@@ -1,1 +1,31 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        int m = arr.length;//rows
+        int n = arr[0].length;
+        int[][] transpose = new int[n][m];
+//Transpose array
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transpose[i][j] = arr[j][i];
+            }
+        }
+        //Reverse the array to get it rotate by 90 degree
+        for (int i = 0; i < transpose.length; i++) {
+            int a = 0;//starting element of the first row of the array
+            int b = m - 1;//last element of the first row of the array
+            while (a < b) {
+                int temp = transpose[i][a];
+                transpose[i][a] = transpose[i][b];
+                transpose[i][b] = temp;
+                a++;
+                b--;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = transpose[i][j];
+            }
+        }
 
+    }
+}
