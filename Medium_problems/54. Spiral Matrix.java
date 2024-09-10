b37a7d1ff885b4315f97068836fd061dffb68ecb
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-09-10 20:16:31 +0530

    Update 54. Spiral Matrix.java

--- Medium_problems/54. Spiral Matrix.java
+++ Medium_problems/54. Spiral Matrix.java
@@ -1,1 +1,33 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] arr) {
+                int n=arr.length, m=arr[0].length;
+List<Integer> ans=new ArrayList<>();
+        int min_r=0, max_r=n-1, min_c=0, max_c=m-1;
 
+      while(min_r<=max_r && min_c<=max_c){
+
+          for (int j=min_c;j<=max_c;j++){
+              ans.add(arr[min_r][j]);
+          } min_r++;
+
+            if (min_r>max_r || min_c>max_c) break;
+
+          for (int i=min_r;i<=max_r;i++){
+              ans.add(arr[i][max_c]);
+          } max_c--;
+
+          if (min_r>max_r || min_c>max_c) break;
+
+          for (int j=max_c;j>=min_c;j--) {
+              ans.add(arr[max_r][j]);
+          } max_r--;
+
+          if (min_r>max_r || min_c>max_c) break;
+          
+          for (int i=max_r;i>=min_r;i--){
+              ans.add(arr[i][min_c]);
+          } min_c++;
+      }
+      return ans;
+    }
+}
