c97cf273d318101941ab6b76c140866ed31a3a62
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-09-11 21:53:56 +0530

    Code Added

--- Easy_problems/268. Missing Number.java
+++ Easy_problems/268. Missing Number.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i <= n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+        
