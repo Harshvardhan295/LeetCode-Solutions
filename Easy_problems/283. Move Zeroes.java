c611ce26cfc860344e45651853ea5927d4dd8061
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-09-16 09:15:49 +0530

    Update 283. Move Zeroes.java

--- Easy_problems/283. Move Zeroes.java
+++ Easy_problems/283. Move Zeroes.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n=arr.length;
+        int noz=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0) noz++;
+        }
+        for (int x=0;x<n-1;x++) { //for passes...
+            for (int i = 0; i < n - 1-x; i++) {
+                if (arr[i]==0) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+}
