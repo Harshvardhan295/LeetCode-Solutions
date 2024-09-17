14cb6af0361e8afafa1175fd1127447cd7d257dd
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-09-17 20:56:10 +0530

    Code Added

--- Hard_problems/25. Reverse Nodes in K-Group.java
+++ Hard_problems/25. Reverse Nodes in K-Group.java
@@ -1,1 +1,43 @@
+class Solution {
+    ListNode getKthNode(ListNode curr, int k) {
+        while (curr != null && k > 0) {
+            curr = curr.next;
+            k--;
+        }
+        return curr;
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) return head;
+
+        ListNode dummy = new ListNode(0); 
+        dummy.next = head;
+        ListNode groupPrev = dummy;
+
+        while (true) {
+            
+            ListNode kthNode = getKthNode(groupPrev, k);
+            if (kthNode == null)
+                break; // If we don't have k nodes, we're done
+
+            ListNode groupNext = kthNode.next; 
+            ListNode prev = kthNode.next; 
+            ListNode curr = groupPrev.next; 
+
+            // Reverse the k nodes
+            while (curr != groupNext) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+
+            ListNode temp = groupPrev.next; 
+            groupPrev.next = kthNode; 
+            groupPrev = temp; 
+        }
+
+        return dummy.next;
+    }
+}
 
