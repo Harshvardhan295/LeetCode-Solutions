aab24157f7a8e06c83d0b7f9227ba3379df8bc1f
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-09-20 19:02:01 +0530

    Code Added

--- Stacks/20. Valid Parentheses.java
+++ Stacks/20. Valid Parentheses.java
@@ -1,1 +1,24 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                st.push(')'); 
+            } else if (c == '{') {
+                st.push('}'); 
+            } else if (c == '[') {
+                st.push(']'); 
+            } else {// If stack is empty or the top of the stack doesn't match the current character
+                if (st.isEmpty() || st.pop() != c) {
+                    return false;
+                }
+            }
+        }
+        
+        return st.isEmpty(); //if empty then true
+    }   
+}
 
