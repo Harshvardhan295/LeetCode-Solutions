8bf0ab53230fe37eb80ed58754c0572af9b3976e
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-01 21:06:44 +0530

    Code Added

--- Hard_problems/4. Median of Two Sorted Arrays.java
+++ Hard_problems/4. Median of Two Sorted Arrays.java
@@ -1,1 +1,31 @@
+/***********Without Binary Search ************/
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int i = 0, j = 0, k = 0;
+        int m = nums1.length;
+        int n = nums2.length;
 
+        int[] mergedArray = new int[m + n];
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                mergedArray[k++] = nums1[i++];
+            } else {
+                mergedArray[k++] = nums2[j++];
+            }
+        }
+        
+        while (i < m) {
+            mergedArray[k++] = nums1[i++];
+        }
+        
+        while (j < n) {
+            mergedArray[k++] = nums2[j++];
+        }
+        int len = mergedArray.length;
+        if ((len) % 2 != 0) {// Odd length
+            return mergedArray[len / 2];
+        } else {// else
+            return (mergedArray[len / 2] + mergedArray[(len / 2) - 1]) / 2.0;
+        }
+    }
+}
