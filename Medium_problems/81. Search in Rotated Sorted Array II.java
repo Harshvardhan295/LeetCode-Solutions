66375e8cb6d8dfa1716cb9517e8ddb9fe77c516b
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-03 19:26:25 +0530

    Code Added

--- Medium_problems/81. Search in Rotated Sorted Array II.java
+++ Medium_problems/81. Search in Rotated Sorted Array II.java
@@ -1,1 +1,41 @@
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            }
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            if (arr[low] <= arr[mid]) {// for left half
+                if (target >= arr[low] && target < arr[mid]) {
+                   //element exists:
+                    high = mid - 1;
+                } else {
+                    //element does not exist:
+                    low = mid + 1;
+                }
+            } else {// for right half
+
+                if (target >= arr[mid] && target <= arr[high]) {
+                    //element exists:
+                    low = mid + 1;
+                } else {
+                    //element does not exist:
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
 
