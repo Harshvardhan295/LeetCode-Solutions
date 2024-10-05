926323d74ec9028bc1cd2263e852ae3f6b6f2bc5
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-05 05:53:59 +0530

    Added another solution

--- Easy_problems/58. Length of Last Word.java
+++ Easy_problems/58. Length of Last Word.java
@@ -1,3 +1,21 @@
+//______________ Method 1 (efficient method)________________
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;//begin with end
+        
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        return length;
+    }
+}
+
 // ___________ Method 2__________________
   class Solution {
     public int lengthOfLastWord(String s) {
