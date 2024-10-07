d222b76c33c28f99ee2c36bd079a49ee07ed68bd
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-07 21:10:56 +0530

    Code Added

--- Easy_problems/1752. Check if Array Is Sorted and Rotated.java
+++ Easy_problems/1752. Check if Array Is Sorted and Rotated.java
@@ -14,3 +14,17 @@ class Solution {
         return true;
     }
 }
+
+// __________________GOOD Method___________________
+// class Solution {
+//     public boolean check(int[] arr) {
+//         int drop = 0;
+//         for(int i = 1; i < arr.length; i++){
+//             if(arr[i - 1] > arr[i]){
+//                 drop++;
+//             }
+//         }
+//         if(arr[0] < arr[arr.length - 1]) drop++;
+//         return drop <= 1;
+//     }
+// }
