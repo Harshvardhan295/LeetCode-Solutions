0b22b256ff6b892cf9762de7b8dd7ba8401fc565
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-08 20:43:26 +0530

    Code Added

--- Easy_problems/217. Contains Duplicate.java
+++ Easy_problems/217. Contains Duplicate.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
+        int n=arr.length;
+        Arrays.sort(arr);
+        int x=arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]==x) return true;
+            else x=arr[i];
+        }
+    return false;
+    }
+}
