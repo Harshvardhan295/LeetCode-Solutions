da4e674ff0fa5b1b6eb12bed4981fcca870a5195
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-10 20:14:42 +0530

    Added Method 2

--- Medium_problems/62. Unique Paths.java
+++ Medium_problems/62. Unique Paths.java
@@ -1,3 +1,5 @@
+//______________________Method 1_________________________
+
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
@@ -12,5 +14,21 @@ class Solution {
             }
         }
         return dp[m-1][n-1];
+    }
+}
+//______________________Method 2_________________________
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] = 1; // First column
+                } else {
+                    dp[j] += dp[j - 1]; // Update for current cell
+                }
+            }
+        }
+        return dp[n - 1]; // Last cell in the 1D array
     }
 }
