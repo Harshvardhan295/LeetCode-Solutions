c77ad8e9a1a4bc9aa0684ed2cec659ffca38cfb2
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-12 18:19:09 +0530

    Code Added

--- Stacks/1944. Number of Visible People in a Queue.java
+++ Stacks/1944. Number of Visible People in a Queue.java
@@ -1,1 +1,24 @@
+class Solution {
+    public int[] canSeePersonsCount(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        Stack<Integer> st = new Stack<>();
+        
+        for (int i = n - 1; i >= 0; i--) {
+            int count = 0;
+            while (!st.isEmpty() && st.peek() <= arr[i]) {
+                st.pop();
+                count++;
+            }
+            
+            if (!st.isEmpty()) {
+                count++;
+            }
+            
+            result[i] = count; 
+            st.push(arr[i]); 
+        }
 
+        return result;
+    }
+}
