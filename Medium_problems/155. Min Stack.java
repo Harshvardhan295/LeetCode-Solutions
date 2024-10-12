483c0369f90d9069e84ec07fa9ccd13f26fd5fec
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-12 07:43:32 +0530

    Method 1 added

--- Medium_problems/155. Min Stack.java
+++ Medium_problems/155. Min Stack.java
@@ -1,1 +1,47 @@
+// __________________Method 1: Using 2 stacks______________________
+class MinStack {
+    Stack<Integer> mainStack;
+    Stack<Integer> minStack;
 
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        mainStack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (!mainStack.isEmpty()) {
+            int popped = mainStack.pop();
+            if (minStack.peek() == popped) {
+                minStack.pop();
+            }
+        }
+    }
+
+    public int top() {
+        if (mainStack.isEmpty()) return -1;
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        if (minStack.isEmpty()) return -1;
+        return minStack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+// ________________________Method 2: using 1 stack_______________________
