f67fe66a690c58945d2337343f2992c25789eb7b
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-12 18:22:21 +0530

    Added two solutions

--- Dynamic Programming/62. Unique Paths.java
+++ Dynamic Programming/62. Unique Paths.java
@@ -1,1 +1,32 @@
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         int dp[][] = new int[m][n];
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 || j==0){
+//                     dp[i][j]=1;
+//                 }
+//                 else{
+//                     dp[i][j]=dp[i-1][j]+dp[i][j-1];
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// }
 
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j == 0) {
+                    dp[j] = 1; // First column
+                } else {
+                    dp[j] += dp[j - 1]; // Update for current cell
+                }
+            }
+        }
+        return dp[n - 1]; // Last cell in the 1D array
+    }
+}
