751802d6a44e9e87418c54b3de948209b36559b5
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-13 18:57:49 +0530

    Solution added using Stacks

--- Easy_problems/234. Palindrome Linked List.java
+++ Easy_problems/234. Palindrome Linked List.java
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+//_________________________ LINKED LISTS ____________________________
 class Solution {
     public ListNode reverseList(ListNode head) {
         // _____________Iterative Method______________
@@ -61,5 +52,41 @@ class Solution {
             j=j.next;
         }
         return true;
+    }
+}
+//_________________________ STACKS ____________________________
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        
+        Stack<Integer> stack = new Stack<>();
+        ListNode temp = head;
+        while (temp != null) {
+            stack.push(temp.val);
+            temp = temp.next;
+        }
+        
+        temp = head;
+        while (temp != null) {
+            if (temp.val != stack.pop()) {
+                return false; 
+            }
+            temp = temp.next;
+        }
+        
+        return true; // List is Palindrome
     }
 }
