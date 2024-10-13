eb0243e322bfbf8eef5407cc771c58e0987df2dd
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-13 06:32:26 +0530

    Code Added

--- Stacks/394. Decode String.java
+++ Stacks/394. Decode String.java
@@ -1,1 +1,39 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
 
+        for (char c : s.toCharArray()) {
+
+            if (Character.isDigit(c)) {
+                // Building the number k
+                k = k * 10 + (c - '0');
+            } 
+            
+            else if (c == '[') {
+                countStack.push(k);  // Push current k
+                stringStack.push(currentString);//(for case:"[a[b]]" we append till "[a" )
+                k = 0;  // Reset for new segment
+                currentString = new StringBuilder();
+            } 
+            
+            else if (c == ']') {
+                int repeatTimes = countStack.pop();//pop the number
+                StringBuilder decodedString = stringStack.pop();
+                for (int i = 0; i < repeatTimes; i++) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } 
+            
+            else {
+                // Append the current character to the current string
+                currentString.append(c);
+            }
+        }
+
+        return currentString.toString();
+    }
+}
