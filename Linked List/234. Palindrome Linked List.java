5fe22afaa635462d7cd771950f4ca1997b310613
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-13 18:58:29 +0530

    Solution Added

--- Linked List/234. Palindrome Linked List.java
+++ Linked List/234. Palindrome Linked List.java
@@ -1,1 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        // _____________Iterative Method______________
+        ListNode current = head;
+        ListNode prev = null;
+        ListNode Next = head;
+        while (current != null) {
+            Next = current.next;
+            current.next = prev;
+            prev = current;
+            current = Next;
+        }
+        return prev;
+    }
 
+    public boolean isPalindrome(ListNode head) {
+        // if (head.next == null)
+        //     return true;
+        // ___________Deep Copy Method___________
+        // ListNode newHead=new ListNode(head.val);
+        // ListNode t1=head.next;
+        // ListNode t2=newHead;
+        // while(t1!=null){//creating deep copy
+        // ListNode temp=new ListNode(t1.val);
+
+        // t2.next=temp;
+        // t2=t2.next;
+        // t1=t1.next;
+        // }
+        // newHead=reverseList(newHead);//reversing deep copy
+        // t1=head;
+        // while(t1!=null){//comparing with the original lists
+        // if(t1.val!=t2.val) return false;
+        // t2=t2.next;
+        // t1=t1.next;
+        // }
+        // return true;
+
+        // __________Reversing the second half____________
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode j=reverseList(slow);
+        ListNode i=head;
+        while(j!=null){
+            if(i.val!=j.val) return false;
+            i=i.next;
+            j=j.next;
+        }
+        return true;
+    }
+}
