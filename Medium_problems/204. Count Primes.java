0b0517376236ac9d332ccf96cb137e0ba77890be
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-14 08:23:04 +0530

    Code Added

--- Medium_problems/204. Count Primes.java
+++ Medium_problems/204. Count Primes.java
@@ -1,1 +1,32 @@
+class Solution {
+    public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
 
+        boolean[] isPrime = new boolean[n];
+        //assume all numbers are prime
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        // mark all multiples of i as false
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        // Count the remaining as prime numbers
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
