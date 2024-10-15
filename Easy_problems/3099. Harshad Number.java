8e577cfc79feb4bd369b930aa90903963bc3ca32
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-15 18:53:36 +0530

    Added Method 1 and Method 2

--- Easy_problems/3099. Harshad Number.java
+++ Easy_problems/3099. Harshad Number.java
@@ -1,1 +1,29 @@
+//_____________Method 1_____________________
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum = 0;
+        int num = x;
+        
+        while(num!=0){
+            sum += num%10;
+            num/=10;
+        }
+        if(x%sum == 0)  return sum;
+        return -1;
+    }
+}
+//_____________Method 2_____________________
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
 
+        String numberStr = Integer.toString(x);
+        int sum = 0;
+        for (int i = 0; i < numberStr.length(); i++) {
+            sum += Character.getNumericValue(numberStr.charAt(i));
+        }
+        if (x % sum == 0)
+            return sum;
+        else
+            return -1;
+    }
+}
