90305a1244d706439c24cd9497d9f4e4b58a9849
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-16 19:34:04 +0530

    Code Added

--- Easy_problems/507. Perfect Number.java
+++ Easy_problems/507. Perfect Number.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum=0;
+        for(int i=1;i<=num/2;i++){
+            if (num%i==0){
+                sum+=i;//sum gets bigger than i
+            }
+            if (sum<i){//check if any number does not divides num,then it returns sum directly.
+                return false;
+            }
+        }
+        return sum==num;
+    }
+}
