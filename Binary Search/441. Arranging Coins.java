1b1695706a4eeb5150de729e44686a4dbd143206
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-17 21:10:58 +0530

    Code Added

--- Binary Search/441. Arranging Coins.java
+++ Binary Search/441. Arranging Coins.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int low = 1;
+        int high = n; 
+        while (low <= high) {
+            int mid = low + (high - low) / 2; 
+            long sum =(long)  mid * (mid + 1) / 2; //Maths logic: sum of n natural numbers
 
+            if (sum == n) {
+                return mid; // Exact number of rows
+            } else if (sum < n) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
+        }
+        return high; // The largest number of complete rows
+    }
+}
