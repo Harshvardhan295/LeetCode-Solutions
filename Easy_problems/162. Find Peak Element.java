d505ce0dc87ff0b3e374a8ffdc654c77065e7935
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-18 11:03:57 +0530

    Update 162. Find Peak Element.java

--- Easy_problems/162. Find Peak Element.java
+++ Easy_problems/162. Find Peak Element.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {//if mid is greater than next element then peak element should be in the left half
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
