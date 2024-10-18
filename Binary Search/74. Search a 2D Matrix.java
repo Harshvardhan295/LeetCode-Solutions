75eb2580e2f04e0196ada27024d852f84a97cec9
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-18 10:41:53 +0530

    Code Added

--- Binary Search/74. Search a 2D Matrix.java
+++ Binary Search/74. Search a 2D Matrix.java
@@ -1,1 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        int rows = arr.length;
+        int cols = arr[0].length;
+        
+        int low = 0;
+        int high = rows * cols - 1;
 
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midValue = arr[mid / cols][mid % cols];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
