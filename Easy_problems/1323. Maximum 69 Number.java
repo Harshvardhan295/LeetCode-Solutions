b797c222fd105e4ef73e80174c19a2bd2753279a
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-21 23:44:58 +0530

    Update 1323. Maximum 69 Number.java

--- Easy_problems/1323. Maximum 69 Number.java
+++ Easy_problems/1323. Maximum 69 Number.java
@@ -1,1 +1,20 @@
-
+class Solution {
+	public int maximum69Number (int num) {
+		String str = Integer.toString(num);
+		String ans="";
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)=='6')
+			{
+				ans+='9';
+				break;
+			}
+			ans+=str.charAt(i);
+		}
+		if(str.length()!=ans.length())
+		{
+			ans+=str.substring(ans.length());
+		}
+		return Integer.parseInt(ans);
+	}
+}
