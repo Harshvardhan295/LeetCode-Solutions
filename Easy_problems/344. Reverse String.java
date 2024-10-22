c4e68af4a6d0ac8a8435bc20b56ac1f2d2bcd6eb
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-22 20:42:13 +0530

    Code Added

--- Easy_problems/344. Reverse String.java
+++ Easy_problems/344. Reverse String.java
@@ -1,1 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length; 
+        
+        for (int i = 0; i < n / 2; i++) {//n/2 ensures that half right swaps half left elements
+            //Swap
+            char temp = s[i]; 
+            s[i] = s[n - 1 - i];
+            s[n - 1 - i] = temp;
+        }
+    }
+}
 
