3aebcfeb6ce5585cbfe861b758cde802896088d8
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-25 21:50:09 +0530

    Added Method 1

--- Easy_problems/367. Valid Perfect Square.java
+++ Easy_problems/367. Valid Perfect Square.java
@@ -1,1 +1,23 @@
+//_________________________Method 1__________________________
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num < 2) return true;
+        
+        long low = 2, high = num / 2, mid, square;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            square = mid * mid;
+            if (square == num) {
+                return true;
+            }
+            if (square > num) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
+//_________________________Method 1__________________________
 
