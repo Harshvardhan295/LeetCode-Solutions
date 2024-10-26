b4de4c62851dd0f6316ade30ab2b0833835709f2
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-26 22:00:43 +0530

    Code Added

--- Dynamic Programming/64. Minimum Path Sum.java
+++ Dynamic Programming/64. Minimum Path Sum.java
@@ -1,1 +1,26 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
 
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+
+        // Fill the rest of the dp table: For each cell, the minimum path sum is determined by taking the minimum of the above or left and then adding the value of the current cell of the grid.
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        // Return the minimum path sum to reach the bottom-right corner of the grid.
+        return dp[m - 1][n - 1];
+    }
+}
