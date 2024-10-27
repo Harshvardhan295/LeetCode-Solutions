6cb62f5e79ddf23a4fd4478db75649915ef01ede
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-27 19:28:11 +0530

    Code Added

--- Medium_problems/15. 3Sum.java
+++ Medium_problems/15. 3Sum.java
@@ -1,1 +1,41 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> listOfLists = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return listOfLists; 
+        }
+        Arrays.sort(nums);
 
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) { //skip duplicates
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    listOfLists.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return listOfLists;
+    }
+}
