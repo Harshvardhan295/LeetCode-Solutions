f6b9cdf774384ecebbab69bbe44daac98e7fd17f
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-10-29 19:19:35 +0530

    Code Added

--- Medium_problems/79. Word Search.java
+++ Medium_problems/79. Word Search.java
@@ -1,1 +1,39 @@
+class Solution {
+    public boolean[][] visited;
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+        visited = new boolean[m][n];
+        if (m * n < word.length()) {//just edge condition
+            return false;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (search(board, word, i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean search(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index) || visited[i][j]) {
+            return false;
+        }
+        visited[i][j] = true;
+
+        boolean found = search(board, word, i + 1, j, index + 1) ||
+                search(board, word, i - 1, j, index + 1) ||
+                search(board, word, i, j + 1, index + 1) ||
+                search(board, word, i, j - 1, index + 1);
+
+        visited[i][j] = false;
+        return found;
+    }
+}
 
