e6d6958000cd9157ca29fb7666cd92f955d824ec
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-02 20:47:00 +0530

    Code Added

--- Easy_problems/88. Merge Sorted Array.java
+++ Easy_problems/88. Merge Sorted Array.java
@@ -1,1 +1,17 @@
+class Solution {
+    public void merge(int[] arr, int m, int[] arr2, int n) {
+        int[] ans = new int[m+n];
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (arr[i] <= arr2[j]) ans[k++] = arr[i++];
+            else ans[k++] = arr2[j++];
+        }
+        while (j < n) ans[k++] = arr2[j++];
+        while (i < m) ans[k++] = arr[i++];
 
+        for(int l=0;l<ans.length;l++){
+            arr[l]=ans[l];
+        }
+    
+    }
+}
