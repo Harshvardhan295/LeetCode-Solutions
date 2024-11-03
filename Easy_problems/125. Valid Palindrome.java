fe4285c4949cb316eccf06d54f6ca0f968c20a99
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-03 15:11:02 +0530

    Code Added

--- Easy_problems/125. Valid Palindrome.java
+++ Easy_problems/125. Valid Palindrome.java
@@ -1,1 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String S = s.toLowerCase();
+        
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            if (Character.isLetterOrDigit(S.charAt(i))) {
+                str.append(S.charAt(i));
+            }
+        }
+        
+        int k = str.length() - 1;
+        int l = 0;
+        while (l < k) {  
+            if (str.charAt(l) != str.charAt(k)) {
+                return false;
+            }
+            l++;
+            k--;
+        }
+        return true;
+    }
+}
 
