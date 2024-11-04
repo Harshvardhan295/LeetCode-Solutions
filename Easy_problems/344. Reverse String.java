cfb676895d27fcafe17d1bedaf7213f1b1bf37b1
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-04 23:23:09 +0530

    Update 344. Reverse String.java

--- Easy_problems/344. Reverse String.java
+++ Easy_problems/344. Reverse String.java
@@ -1,3 +1,4 @@
+
 class Solution {
     public void reverseString(char[] s) {
         int n = s.length; 
@@ -10,4 +11,18 @@ class Solution {
         }
     }
 }
-
+//---------------Brute Force---------------------
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length; 
+        char[] reversed = new char[n]; 
+        
+        for (int i = 0; i < n; i++) {
+            reversed[i] = s[n - 1 - i]; 
+        }
+        
+        for (int i = 0; i < n; i++) {
+            s[i] = reversed[i];
+        }
+    }
+}
