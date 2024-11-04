536fbc375e35930190eb5b42978f98d082aada3d
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-04 23:26:09 +0530

    Code Added

--- Medium_problems/53. Maximum Subarray.java
+++ Medium_problems/53. Maximum Subarray.java
@@ -16,3 +16,23 @@ class Solution {
         return maxsum;
     }
 }
+//-------------------Brute Force----------------
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int maxsum = Integer.MIN_VALUE; 
+        
+        for (int i = 0; i < n; i++) {
+            int sum = 0; 
+            
+            for (int j = i; j < n; j++) {
+                sum += nums[j]; 
+                if (sum > maxsum) {
+                    maxsum = sum;
+                }
+            }
+        }
+        
+        return maxsum;
+    }
+}
