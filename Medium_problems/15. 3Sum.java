85dd867280c322b3312bb26f60c6770e293c06a1
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-07 21:05:42 +0530

    Update 15. 3Sum.java

--- Medium_problems/15. 3Sum.java
+++ Medium_problems/15. 3Sum.java
@@ -1,3 +1,4 @@
+//Optimised approach
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> listOfLists = new ArrayList<>();
@@ -39,3 +40,26 @@ class Solution {
         return listOfLists;
     }
 }
+//Brute Force
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                        triplet.sort(Integer::compareTo);
+                        
+                        if (!result.contains(triplet)) {
+                            result.add(triplet);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+}}
