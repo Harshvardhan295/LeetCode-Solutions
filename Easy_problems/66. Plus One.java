72c6c490dc802a0d69c084b64187b4e819925c43
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-08 22:09:44 +0530

    Update 66. Plus One.java

--- Easy_problems/66. Plus One.java
+++ Easy_problems/66. Plus One.java
@@ -1,3 +1,4 @@
+//Optimised Code
 class Solution {
     public int[] plusOne(int[] arr) {
         int n = arr.length;
@@ -14,5 +15,24 @@ class Solution {
         int[] newArr = new int[n + 1];
         newArr[0] = 1; 
         return newArr;
+    }
+}
+//Brute Force
+class Solution {
+    public int[] plusOne(int[] arr) {
+        int num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            num = num * 10 + arr[i];
+        }
+        
+        num += 1;
+        
+        String str = Integer.toString(num);
+        int[] result = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            result[i] = str.charAt(i) - '0';
+        }
+        
+        return result;
     }
 }
