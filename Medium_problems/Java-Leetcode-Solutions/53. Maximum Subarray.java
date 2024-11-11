b64b67c3ac308a32e7186877b5c1b21506d53335
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-12 04:34:35 +0530

    Update 53. Maximum Subarray.java

--- Medium_problems/Java-Leetcode-Solutions/53. Maximum Subarray.java
+++ Medium_problems/Java-Leetcode-Solutions/53. Maximum Subarray.java
@@ -1,1 +1,38 @@
-
+//-------------------Optimised code----------------
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int maxsum=nums[0];
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>maxsum){
+                maxsum=sum;
+            }
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxsum;
+    }
+}
+//-------------------Brute Force----------------
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int maxsum = Integer.MIN_VALUE; 
+        
+        for (int i = 0; i < n; i++) {
+            int sum = 0; 
+            
+            for (int j = i; j < n; j++) {
+                sum += nums[j]; 
+                if (sum > maxsum) {
+                    maxsum = sum;
+                }
+            }
+        }
+        
+        return maxsum;
+    }
+}
