4bad3e4e5930adb91ffcd68d3fb73a71a81d9b1e
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-14 21:47:13 +0530

    Update 206. Reverse Linked List.java

--- Easy_problems/C-LeetCode-Solutions/206. Reverse Linked List.java
+++ Easy_problems/C-LeetCode-Solutions/206. Reverse Linked List.java
@@ -1,1 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    //--------------iterative--------------
+        // struct ListNode* current=head;
+        // struct ListNode* prev=NULL;
+        // struct ListNode* Next=head;
+        // while(current!=NULL){
+        // Next=current->next;
+        // current->next=prev;
+        // prev=current;
+        // current=Next;
+        // }
+        // return prev;
 
+
+}
