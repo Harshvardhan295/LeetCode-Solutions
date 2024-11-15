11a1dd00650f0ea9c88e25e70ca462bb194b25d7
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-15 18:48:40 +0530

    Update 58. Length of Last Word.C

--- Easy_problems/C-LeetCode-Solutions/58. Length of Last Word.C
+++ Easy_problems/C-LeetCode-Solutions/58. Length of Last Word.C
@@ -1,1 +1,15 @@
+int lengthOfLastWord(char* s) {
+    int length = 0;
+    int i = strlen(s) - 1; // Get the index of the last character
+
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+    return length;
+}
 
