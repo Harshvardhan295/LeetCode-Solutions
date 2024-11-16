9a6e4e7cc08c80ad1b75329646246c591c60a128
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-16 23:22:33 +0530

    Update 50. Pow(x, n).C

--- Medium_problems/C-Leetcode-Solutions/50. Pow(x, n).C
+++ Medium_problems/C-Leetcode-Solutions/50. Pow(x, n).C
@@ -1,1 +1,20 @@
+double powHelper(double x, long n) {
+    if (n == 0) return 1;
+    double half = powHelper(x, n / 2);
+    if (n % 2 == 0) {
+        return half * half;
+    } else {
+        return half * half * x;
+    }
+}
+
+double myPow(double x, int n) {
+    if (n == 0) return 1;
+    long longN = n;
+    if (longN < 0) {
+        x = 1 / x;
+        longN = -longN;  
+        }
+    return powHelper(x, longN);
+}
 
