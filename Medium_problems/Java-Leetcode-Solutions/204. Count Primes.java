80020ca53e0b654628d73e3ae988d78a7e1d6bb1
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-17 13:15:42 +0530

    Update 204. Count Primes.java

--- Medium_problems/Java-Leetcode-Solutions/204. Count Primes.java
+++ Medium_problems/Java-Leetcode-Solutions/204. Count Primes.java
@@ -13,7 +13,7 @@ class Solution {
         // mark all multiples of i as false
         for (int i = 2; i * i < n; i++) {
             if (isPrime[i]) {
-                for (int j = i * i; j < n; j += i) {
+                for (int j = i * i; j < n; j += i) {//if its square lies in range less than n then it is not prime
                     isPrime[j] = false;
                 }
             }
