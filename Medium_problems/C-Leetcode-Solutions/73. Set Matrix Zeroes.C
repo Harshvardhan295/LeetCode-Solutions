fda67388bf1b9304bfd2fead61804897fd10f46a
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-20 21:25:30 +0530

    Code Added

--- Medium_problems/C-Leetcode-Solutions/73. Set Matrix Zeroes.C
+++ Medium_problems/C-Leetcode-Solutions/73. Set Matrix Zeroes.C
@@ -1,1 +1,35 @@
+void setZeroes(int** arr, int arrSize, int* arrColSize) {
+    int m = arrSize, n = *arrColSize;
+
+    int* rowFlags = (int*)calloc(m, sizeof(int)); // Rows to be zeroed
+    int* colFlags = (int*)calloc(n, sizeof(int)); // Columns to be zeroed
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[i][j] == 0) {
+                rowFlags[i] = 1; 
+                colFlags[j] = 1; 
+            }
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        if (rowFlags[i]) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    for (int j = 0; j < n; j++) {
+        if (colFlags[j]) {
+            for (int i = 0; i < m; i++) {
+                arr[i][j] = 0;
+            }
+        }
+    }
+
+    free(rowFlags);
+    free(colFlags);
+}
 
