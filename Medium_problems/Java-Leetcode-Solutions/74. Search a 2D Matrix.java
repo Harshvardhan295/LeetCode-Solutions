53a4e088f32456aff9b2d498a2ee3554d13a7f55
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-21 18:19:40 +0530

    Update 74. Search a 2D Matrix.java

--- Medium_problems/Java-Leetcode-Solutions/74. Search a 2D Matrix.java
+++ Medium_problems/Java-Leetcode-Solutions/74. Search a 2D Matrix.java
@@ -1,19 +1,23 @@
-bool searchMatrix(int** arr, int arrSize, int* arrColSize, int target) {
-    int m = arrSize;
-    int n = *arrColSize;
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        int rows = arr.length;
+        int cols = arr[0].length;
+        
+        int low = 0;
+        int high = rows * cols - 1;
 
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if (arr[i][j] == target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int midValue = arr[mid / cols][mid % cols];
+
+            if (midValue == target) {
                 return true;
-            }
-
-            if (arr[i][j] > target) {
-                break;  // No need to check further in this row
+            } else if (midValue < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
+        return false;
     }
-
-    return false;  
 }
-
