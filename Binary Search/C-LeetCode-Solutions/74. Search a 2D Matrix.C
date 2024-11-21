229fef8d13482ad7c52a0f8b487f8b942badc602
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-21 18:26:17 +0530

    Update 74. Search a 2D Matrix.C

--- Binary Search/C-LeetCode-Solutions/74. Search a 2D Matrix.C
+++ Binary Search/C-LeetCode-Solutions/74. Search a 2D Matrix.C
@@ -1,1 +1,17 @@
+bool searchMatrix(int** arr, int arrSize, int* arrColSize, int target) {
+    int m = arrSize;
+    int n = *arrColSize;
 
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (arr[i][j] == target) {
+                return true;
+            }
+
+            if (arr[i][j] > target) {
+                break;  // No need to check further in this row
+            }
+        }
+    }
+    return false;  
+}
