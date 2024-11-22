ed157ecf019f81e9451fd30a275996d7f5106d2b
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-22 20:13:43 +0530

    Update 74. Search a 2D Matrix.java

--- Binary Search/Java-LeetCode-Solutions/74. Search a 2D Matrix.java
+++ Binary Search/Java-LeetCode-Solutions/74. Search a 2D Matrix.java
@@ -1,1 +1,15 @@
+bool searchMatrix(int** arr, int arrSize, int* arrColSize, int target) {
+    int i = 0;        
+    int j = * arrColSize - 1; 
 
+    while (i < arrSize && j >= 0) {
+        if (arr[i][j] == target) {
+            return true; 
+        } else if (arr[i][j] < target) {
+            i++; 
+        } else {
+            j--; 
+        }
+    }
+    return false; 
+}
