c40398e51ce7e439239f24425088a531a26fecb3
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-23 23:31:57 +0530

    Code Added

--- Easy_problems/C-LeetCode-Solutions/136. Single Number.C
+++ Easy_problems/C-LeetCode-Solutions/136. Single Number.C
@@ -1,1 +1,15 @@
-
+int singleNumber(int* nums, int n) {
+    for (int i = 0; i < n; i++) {
+        int flag = 1;
+        for (int j = 0; j < n; j++) {
+            if (i != j && nums[i] == nums[j]) {
+                flag = 0; 
+                break;
+            }
+        }
+        if (flag == 1) { 
+            return nums[i];
+        }
+    }
+    return -1; 
+}
