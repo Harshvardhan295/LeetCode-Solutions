9052c9102e60868043ececb7e180dc08d88192e0
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-24 22:29:39 +0530

    Update 852. Peak Index in a Mountain Array.C

--- Medium_problems/C-Leetcode-Solutions/852. Peak Index in a Mountain Array.C
+++ Medium_problems/C-Leetcode-Solutions/852. Peak Index in a Mountain Array.C
@@ -1,1 +1,13 @@
-
+int peakIndexInMountainArray(int* arr, int n) {
+    int low = 0;
+    int high = n - 1;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] < arr[mid + 1]) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    return low;
+}
