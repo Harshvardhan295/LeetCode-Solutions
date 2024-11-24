cf52d5a5bd3d8dfbb1b22cba4a1654248765962b
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-24 22:33:13 +0530

    Code Added

--- Binary Search/C-LeetCode-Solutions/852. Peak Index in a Mountain Array.C
+++ Binary Search/C-LeetCode-Solutions/852. Peak Index in a Mountain Array.C
@@ -1,19 +1,13 @@
-class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-        int n = arr.length;
-        int low = 1;
-        int high = n - 2;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) {
-                return mid;
-            } else if (arr[mid] < arr[mid-1]) {
-                high = mid - 1;
-            } else { // arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]
-                low = mid + 1;
-            }
+int peakIndexInMountainArray(int* arr, int n) {
+    int low = 0;
+    int high = n - 1;
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] < arr[mid + 1]) {
+            low = mid + 1;
+        } else {
+            high = mid;
         }
-        return -1; // This should never be reached if the input is a valid mountain array
     }
+    return low;
 }
