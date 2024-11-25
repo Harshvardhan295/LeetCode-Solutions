2a03661fafda6b89173ee812343b3665461b9eec
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-25 22:22:45 +0530

    Update 704. Binary Search.C

--- Easy_problems/C-LeetCode-Solutions/704. Binary Search.C
+++ Easy_problems/C-LeetCode-Solutions/704. Binary Search.C
@@ -1,1 +1,16 @@
+int search(int* arr, int numsSize, int target) {
+     int low=0;
+            int high = numsSize-1;
+            while (low <= high) {
+                int mid = low + (high - low) / 2; // To avoid overflow
 
+                if (arr[mid] == target) {
+                    return mid;
+                } else if (arr[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+                return -1;
+}
