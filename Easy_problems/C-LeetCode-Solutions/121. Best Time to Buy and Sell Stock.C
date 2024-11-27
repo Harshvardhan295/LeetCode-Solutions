a1ec2135249c2c79a0e77780a3f0b13a13d4d334
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-27 20:46:37 +0530

    Update 121. Best Time to Buy and Sell Stock.C

--- Easy_problems/C-LeetCode-Solutions/121. Best Time to Buy and Sell Stock.C
+++ Easy_problems/C-LeetCode-Solutions/121. Best Time to Buy and Sell Stock.C
@@ -1,1 +1,12 @@
-
+int maxProfit(int* arr, int n) {
+    int min = INT_MAX;
+    int maxProfit = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        } else if (arr[i] - min > maxProfit) {
+            maxProfit = arr[i] - min;
+        }
+    }
+    return maxProfit;
+}
