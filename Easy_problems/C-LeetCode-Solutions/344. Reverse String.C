891364a77c693b9c1edbd5b29545796ee74eecc5
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-29 20:29:44 +0530

    Code Added

--- Easy_problems/C-LeetCode-Solutions/344. Reverse String.C
+++ Easy_problems/C-LeetCode-Solutions/344. Reverse String.C
@@ -1,1 +1,11 @@
-
+void reverseString(char* s, int size) {
+    int i=0;
+    int j=size-1;
+    while(i<=j){
+        char temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+    }
+}
