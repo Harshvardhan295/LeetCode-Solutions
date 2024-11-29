4beb9dc47c7d2b3c2a0c14d9dd19b5395aaf99ce
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-29 20:30:32 +0530

    Code Added

--- Easy_problems/Java-Leetcode-Solutions/344. Reverse String.java
+++ Easy_problems/Java-Leetcode-Solutions/344. Reverse String.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length; 
+        char[] reversed = new char[n]; 
+        
+        for (int i = 0; i < n; i++) {
+            reversed[i] = s[n - 1 - i]; 
+        }
+        
+        for (int i = 0; i < n; i++) {
+            s[i] = reversed[i];
+        }
+    }
+}
