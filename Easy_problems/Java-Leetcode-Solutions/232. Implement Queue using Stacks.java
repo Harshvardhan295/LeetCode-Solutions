7367bd957f5156868f7ec5884ec691226c889826
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-30 20:54:24 +0530

    Update 232. Implement Queue using Stacks.java

--- Easy_problems/Java-Leetcode-Solutions/232. Implement Queue using Stacks.java
+++ Easy_problems/Java-Leetcode-Solutions/232. Implement Queue using Stacks.java
@@ -1,1 +1,35 @@
+class MyQueue {
+     Stack<Integer> st;
+     Stack<Integer> q;
 
+    public MyQueue() {
+        st = new Stack<>();
+        q = new Stack<>();
+    }
+    
+    public void push(int x) {
+        st.push(x);
+    }
+    
+    public int pop() {
+        if (q.isEmpty()) {
+            while (!st.isEmpty()) {
+                q.push(st.pop());
+            }
+        }
+        return q.pop();
+    }
+    
+    public int peek() {
+        if (q.isEmpty()) {
+            while (!st.isEmpty()) {
+                q.push(st.pop());
+            }
+        }
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return st.isEmpty() && q.isEmpty();
+    }
+}
