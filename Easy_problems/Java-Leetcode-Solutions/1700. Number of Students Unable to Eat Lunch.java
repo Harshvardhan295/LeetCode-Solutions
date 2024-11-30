4f71b5c989e261e05a69827e88ea51e1d202edef
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-30 20:53:21 +0530

    Update 1700. Number of Students Unable to Eat Lunch.java

--- Easy_problems/Java-Leetcode-Solutions/1700. Number of Students Unable to Eat Lunch.java
+++ Easy_problems/Java-Leetcode-Solutions/1700. Number of Students Unable to Eat Lunch.java
@@ -1,1 +1,37 @@
+import java.util.*;
 
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> st = new Stack<>();
+        int count = 0;
+
+        for (int i = sandwiches.length - 1; i >= 0; i--) {
+            st.push(sandwiches[i]);
+        }
+
+        for (int i = 0; i < students.length; i++) {
+            q.add(students[i]);
+        }
+        
+        int rotations = 0; // Initialize a variable to count the number of rotations without serving a sandwich
+
+        while (!st.isEmpty()) {
+            if (q.peek() == st.peek()) {
+                st.pop();
+                q.poll();
+                count++; 
+                rotations = 0; // Reset rotations since we served a sandwich
+            } else {
+                q.add(q.poll());
+                rotations++;
+            }
+
+            // If no progress has been made in a full rotation, break out of the loop
+            if (rotations == students.length) {
+                break;
+            }
+        }
+        return students.length - count;
+    }
+}
