c55e76fac41b13341bd6bffec6e771eb3679ea35
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-11-30 20:55:37 +0530

    Update 344. Reverse String.java

--- Easy_problems/Java-Leetcode-Solutions/344. Reverse String.java
+++ Easy_problems/Java-Leetcode-Solutions/344. Reverse String.java
@@ -1,3 +1,17 @@
+//--------------------Optimised solution-----------------------
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length; 
+        
+        for (int i = 0; i < n / 2; i++) {//n/2 ensures that half right swaps half left elements
+            //Swap
+            char temp = s[i]; 
+            s[i] = s[n - 1 - i];
+            s[n - 1 - i] = temp;
+        }
+    }
+}
+//---------------Brute Force---------------------
 class Solution {
     public void reverseString(char[] s) {
         int n = s.length; 
