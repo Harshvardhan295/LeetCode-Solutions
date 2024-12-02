f65d3fa6002736d980daf964095849cb7aee76d2
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-02 20:52:04 +0530

    Code Added

--- Easy_problems/C-LeetCode-Solutions/1700. Number of Students Unable to Eat Lunch.C
+++ Easy_problems/C-LeetCode-Solutions/1700. Number of Students Unable to Eat Lunch.C
@@ -1,1 +1,26 @@
-
+int countStudents(int* students, int studentsSize, int* sandwiches, int sandwichesSize) {
+    int zeroes = 0, ones = 0;
+    for (int i = 0; i < studentsSize; i++) {
+        if (students[i] == 0) {
+            zeroes++;
+        } else {
+            ones++;
+        }
+    }
+    for (int i = 0; i < sandwichesSize; i++) {
+        if (sandwiches[i] == 0) {
+            if (zeroes > 0) {
+                zeroes--;
+            } else {
+                break;
+            }
+        } else {
+            if (ones > 0) {
+                ones--;
+            } else {
+                break;
+            }
+        }
+    }
+    return zeroes + ones;
+}
