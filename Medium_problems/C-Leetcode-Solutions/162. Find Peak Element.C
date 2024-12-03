586624f608abf198f53e6948a11da2868e94de5c
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-03 19:55:57 +0530

    Code Added

--- Medium_problems/C-Leetcode-Solutions/162. Find Peak Element.C
+++ Medium_problems/C-Leetcode-Solutions/162. Find Peak Element.C
@@ -1,3 +1,4 @@
+//Code with edge conditions
 int findPeakElement(int* arr, int n) {
     if (n == 1) return 0;  
     if (arr[0] > arr[1]) return 0;  
@@ -19,4 +20,19 @@ int findPeakElement(int* arr, int n) {
     }
 
     return -1;  // This should not happen for valid input
+}
+//optimised code
+int findPeakElement(int* nums, int n) {
+     int low = 0;
+        int high = n - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {//if mid is greater than next element then peak element should be in the left half
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
 }
