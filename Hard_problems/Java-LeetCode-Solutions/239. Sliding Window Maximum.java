4f258dbd9f1d34b28a4cbfe0f8ae263d85a47a98
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-06 22:30:13 +0530

    C

--- Hard_problems/Java-LeetCode-Solutions/239. Sliding Window Maximum.java
+++ Hard_problems/Java-LeetCode-Solutions/239. Sliding Window Maximum.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] maxarr = new int[nums.length - k + 1];
+        
+        for (int i = 0; i <= nums.length - k; i++) {
+            int max = Integer.MIN_VALUE;
+            
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            
+            maxarr[i] = max;
+        }
+        
+        return maxarr;
+    }
+}
