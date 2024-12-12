951257cc3e029e7a0ea624c45f7a39beb17aaa01
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-12 20:53:49 +0530

    added solution

--- Medium_problems/Java-Leetcode-Solutions/950. Reveal Cards In Increasing Order.java
+++ Medium_problems/Java-Leetcode-Solutions/950. Reveal Cards In Increasing Order.java
@@ -1,1 +1,17 @@
-
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* deckRevealedIncreasing(int* deck, int n, int* returnSize) {
+     qsort(deck, n, sizeof(int), compare);
+     Queue<Integer> q=new LinkedList<>();
+    int[] res=new int[n];
+     for(int i=0;i<n;i++){
+        q.add(i);
+     }
+     int i=0;
+    while(!q.isEmpty()){
+        res[q.poll()]=deck[i++];
+        q.add(q.poll());
+    }
+    return res;
+}
