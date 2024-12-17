e347846b155ed9e0c38b4e76329b526bdcc3a284
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-17 22:05:41 +0530

    Update 257. Binary Tree Paths.java

--- Easy_problems/Java-Leetcode-Solutions/257. Binary Tree Paths.java
+++ Easy_problems/Java-Leetcode-Solutions/257. Binary Tree Paths.java
@@ -1,1 +1,17 @@
-
+class Solution {
+public static void paths(TreeNode root,String s,List<String> a){
+    if(root ==null) return;
+    if(root.left==null && root.right==null){//leaf node
+        s+=root.val;
+        a.add(s);
+        return;
+    }
+    paths(root.left,s+root.val+"->",a);
+    paths(root.right,s+root.val+"->",a);
+}
+    public List<String> binaryTreePaths(TreeNode root) {
+       List<String> a=new ArrayList<>();
+       paths(root,"",a);
+       return a;
+    }
+}
