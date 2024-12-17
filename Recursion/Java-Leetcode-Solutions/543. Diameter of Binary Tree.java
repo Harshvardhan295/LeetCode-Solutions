054f29b37291e814da1e6539bb5bdcc3790a4131
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-17 22:10:32 +0530

    Update 543. Diameter of Binary Tree.java

--- Recursion/Java-Leetcode-Solutions/543. Diameter of Binary Tree.java
+++ Recursion/Java-Leetcode-Solutions/543. Diameter of Binary Tree.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int levels(TreeNode root) {
+        if (root == null) 
+            return 0;
+        return 1 + Math.max(levels(root.left), levels(root.right));
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int diameter = levels(root.left) + levels(root.right);
+        int leftHeight = diameterOfBinaryTree(root.left);
+        int rightHeight = diameterOfBinaryTree(root.right);
+        return Math.max(diameter, Math.max(leftHeight, rightHeight));
+    }
+}
 
