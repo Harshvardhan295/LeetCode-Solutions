47ffcf3584cb112a7db1a41a8c2a1359678c6883
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-19 22:29:32 +0530

    Update 102. Binary Tree Level Order Traversal.java

--- Medium_problems/Java-Leetcode-Solutions/102. Binary Tree Level Order Traversal.java
+++ Medium_problems/Java-Leetcode-Solutions/102. Binary Tree Level Order Traversal.java
@@ -1,1 +1,35 @@
+//Method 1
+class Solution {
+    public int levels(TreeNode root) {
+        if (root == null)
+            return 0;
+        return 1 + Math.max(levels(root.left), levels(root.right));
+    }
+
+    public void nthLevel(TreeNode root, int level, int n, List<Integer> arr) {
+        if (root == null)
+            return;
+
+        if (level == n) {
+            arr.add(root.val);
+            return;
+        }
+
+        nthLevel(root.left, level + 1, n, arr);
+        nthLevel(root.right, level + 1, n, arr);
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = levels(root); // Total number of levels
+
+        // Traverse through all levels
+        for (int i = 0; i < n; i++) {
+            List<Integer> arr = new ArrayList<>();
+            nthLevel(root, 0, i, arr); 
+            ans.add(arr);
+        }
+        return ans;
+    }
+}
 
