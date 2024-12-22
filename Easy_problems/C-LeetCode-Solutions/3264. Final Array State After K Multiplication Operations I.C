9382f16f1a6eb9546110976804c801fe84db9427
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-22 22:49:00 +0530

    Code Added

--- Easy_problems/C-LeetCode-Solutions/3264. Final Array State After K Multiplication Operations I.C
+++ Easy_problems/C-LeetCode-Solutions/3264. Final Array State After K Multiplication Operations I.C
@@ -1,1 +1,25 @@
 
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* getFinalState(int* nums, int numsSize, int k, int multiplier,
+                   int* returnSize) {
+    while (k > 0) {
+        int min = INT_MAX;
+        int minIdx = -1;
+
+        for (int i = 0; i < numsSize; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+                minIdx = i;
+            }
+        }
+
+        nums[minIdx] = nums[minIdx] * multiplier;
+
+        k--;
+    }
+
+    *returnSize = numsSize;
+    return nums;
+}
