760f3c7bb2e8097488c0253e41ff7ac428b5b280
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-23 16:39:51 +0530

    Update 3264. Final Array State After K Multiplication Operations I.java

--- Easy_problems/Java-Leetcode-Solutions/3264. Final Array State After K Multiplication Operations I.java
+++ Easy_problems/Java-Leetcode-Solutions/3264. Final Array State After K Multiplication Operations I.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+    while (k > 0) {
+        int min = Integer.MAX_VALUE;
+        int minIdx = -1;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+                minIdx = i;
+            }
+        }
+
+        nums[minIdx] = nums[minIdx] * multiplier;
+
+        k--;
+    }
+
+    return nums;
+
+    }
+}
