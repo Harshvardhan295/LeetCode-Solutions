4f53be44ce6d4761d081d0109b3ae35fe3844f32
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-24 17:10:45 +0530

    Update 162. Find Peak Element.C

--- Easy_problems/C-LeetCode-Solutions/162. Find Peak Element.C
+++ Easy_problems/C-LeetCode-Solutions/162. Find Peak Element.C
@@ -1,1 +1,14 @@
-
+int findPeakElement(int* nums, int n) {
+     int low = 0;
+        int high = n - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+}
