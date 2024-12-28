b721663a49b4494b7b744fc05ac8460758d9c161
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-28 22:15:04 +0530

    Code Added

--- Medium_problems/C-Leetcode-Solutions/155. Min Stack.C
+++ Medium_problems/C-Leetcode-Solutions/155. Min Stack.C
@@ -1,1 +1,30 @@
+class MinStack {
+    Stack<Integer> mainStack;
+int min;
+    public MinStack() {
+        mainStack = new Stack<>();
+        min=Integer.MAX_VALUE;
+    }
 
+    public void push(int val) {
+        if ( val <= min) {
+            mainStack.push(min);//push current min again before updating min
+            min=val;//update minimum
+        }
+        mainStack.push(val);
+    }
+
+    public void pop() {
+        if (mainStack.pop()==min) {
+            min=mainStack.pop();//if poped element is equal to  min , pop again 
+        }
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
