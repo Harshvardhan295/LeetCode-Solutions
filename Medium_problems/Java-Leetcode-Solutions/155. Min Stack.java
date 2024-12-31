36b676422479fcfa6c4ce95862cc3807f9c8c2c3
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2024-12-31 22:51:38 +0530

    Method 1 added

--- Medium_problems/Java-Leetcode-Solutions/155. Min Stack.java
+++ Medium_problems/Java-Leetcode-Solutions/155. Min Stack.java
@@ -29,4 +29,35 @@ int min;
         return min;
     }
 }
+//Mehtod 2 using Stacks
+class MinStack {
+    Stack<Integer> mainStack;
+int min;
+    public MinStack() {
+        mainStack = new Stack<>();
+        min=Integer.MAX_VALUE;
+    }
+
+    public void push(int val) {
+        if ( val <= min) {
+            mainStack.push(min);//push current min again before updating min
+            min=val;//update minimum
+        }
+        mainStack.push(val);
+    }
+
+    public void pop() {
+        if (mainStack.pop()==min) {
+            min=mainStack.pop();//if poped element is equal to  min , pop again and put this poped element in min
+        }
+    }
+
+    public int top() {
+        return mainStack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
 
