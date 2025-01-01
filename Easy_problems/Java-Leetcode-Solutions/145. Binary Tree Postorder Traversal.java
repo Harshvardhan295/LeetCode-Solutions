91df270e64ae98a73f021268b3ceca21ba92e63e
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-01 22:12:10 +0530

    Update 145. Binary Tree Postorder Traversal.java

--- Easy_problems/Java-Leetcode-Solutions/145. Binary Tree Postorder Traversal.java
+++ Easy_problems/Java-Leetcode-Solutions/145. Binary Tree Postorder Traversal.java
@@ -9,3 +9,23 @@ class Solution {
         return list;//return accumative result
     }
 }
+//Iterative Method
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if (root != null)
+            st.push(root);
+        while (!st.isEmpty()) {
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if (top.left != null)
+                st.push(top.left);
+            if (top.right != null)
+                st.push(top.right); 
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
