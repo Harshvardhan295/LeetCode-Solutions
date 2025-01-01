0f992daf9c6b127333641e62bcf422fcbe4da48a
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-01 22:13:24 +0530

    Update 144. Binary Tree Preorder Traversal.java

--- Easy_problems/Java-Leetcode-Solutions/144. Binary Tree Preorder Traversal.java
+++ Easy_problems/Java-Leetcode-Solutions/144. Binary Tree Preorder Traversal.java
@@ -1,1 +1,11 @@
-
+//Recursive Method
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;// Base case: if the node is null, return empty list
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;//return accumulated result
+    }
+}
