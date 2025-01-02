1fbd7ae24364ee64ec76bf85605db7901555b6e9
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-02 23:51:57 +0530

    Added Method 2

--- Easy_problems/Java-Leetcode-Solutions/94. Binary Tree Inorder Traversal.java
+++ Easy_problems/Java-Leetcode-Solutions/94. Binary Tree Inorder Traversal.java
@@ -1,1 +1,33 @@
+//Recursive Method
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;//base case: if it is leaf node
+        list.addAll(inorderTraversal(root.left));
+        list.add(root.val);
+        list.addAll(inorderTraversal(root.right));
+        return list;//return accumative result
+    }
+}
+//Iterative Method
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode temp=root;
+        while(true){
+            if(temp!=null){
+                st.push(temp);
+                temp=temp.left;
+            }
+            else{
+                if(st.isEmpty()) break;
+                TreeNode top=st.pop();
+                ans.add(top.val);
+                temp=top.right;
+            }
+        }
+        return ans;
+    }
+}
 
