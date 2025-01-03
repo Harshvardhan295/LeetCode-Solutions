ee26752ab0284eb05b907bd3983eed1ff21286a7
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-03 22:31:59 +0530

    Update 110. Balanced Binary Tree.java

--- Easy_problems/Java-Leetcode-Solutions/110. Balanced Binary Tree.java
+++ Easy_problems/Java-Leetcode-Solutions/110. Balanced Binary Tree.java
@@ -1,1 +1,17 @@
+class Solution {
+    public static int levels(TreeNode root,boolean[] ans) {
+        if (root == null)
+            return 0;
+        int leftlevels=levels(root.left,ans);
+        int rightlevels=levels(root.right,ans);
+        int diff=Math.abs(leftlevels-rightlevels);
+        if(diff>1) ans[0]=false;
+        return 1 + Math.max(leftlevels,rightlevels);
+    }
 
+    public boolean isBalanced(TreeNode root) {
+        boolean[] ans={true};
+        levels(root,ans);
+        return ans[0];
+    }
+}
