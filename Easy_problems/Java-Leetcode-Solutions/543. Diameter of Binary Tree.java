ee7321408cb9737e342715590c61ab11033f2e3a
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-03 22:36:46 +0530

    Added methods

--- Easy_problems/Java-Leetcode-Solutions/543. Diameter of Binary Tree.java
+++ Easy_problems/Java-Leetcode-Solutions/543. Diameter of Binary Tree.java
@@ -16,24 +16,7 @@ class Solution {
         return Math.max(diameter, Math.max(leftHeight, rightHeight));
     }
 }
-// Optimised
-class Solution {
-    public int diameter = 0;
 
-    public int diameterOfBinaryTree(TreeNode root) {
-        calculateHeight(root);
-        return diameter;
-    }
-    private int calculateHeight(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-        int leftHeight = calculateHeight(node.left);
-        int rightHeight = calculateHeight(node.right);
-        diameter = Math.max(diameter, leftHeight + rightHeight);
-        return 1 + Math.max(leftHeight, rightHeight);
-    }
-}
 // Optimised Code
 class Solution {
     public int levels(TreeNode root, int[] maxDia) {
@@ -50,5 +33,23 @@ class Solution {
         int[] maxDia = {0};
         levels(root, maxDia);
         return maxDia[0];
+    }
+}
+// Optimised
+class Solution {
+    public int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        calculateHeight(root);
+        return diameter;
+    }
+    private int calculateHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = calculateHeight(node.left);
+        int rightHeight = calculateHeight(node.right);
+        diameter = Math.max(diameter, leftHeight + rightHeight);
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
