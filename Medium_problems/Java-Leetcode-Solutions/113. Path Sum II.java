7a496075d3c4faa5ad69a334af6e10a7d70bb1b6
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-05 20:36:49 +0530

    Update 113. Path Sum II.java

--- Medium_problems/Java-Leetcode-Solutions/113. Path Sum II.java
+++ Medium_problems/Java-Leetcode-Solutions/113. Path Sum II.java
@@ -1,1 +1,35 @@
+//Code using shallow copy
+class Solution {
+    public static List<Integer> copy(List<Integer> arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int ele : arr) {
+            list.add(ele);
+        }
+        return list;
+    }
+
+    public static void helper(TreeNode root, int sum, List<Integer> arr, List<List<Integer>> ans) {
+        if (root == null)
+            return;
+        if (root.left == null && root.right == null){
+            if(root.val == sum){
+            arr.add(root.val);
+            ans.add(arr);
+            }
+            return;
+    }
+        arr.add(root.val);
+        List<Integer> arr1 = copy(arr);
+        List<Integer> arr2 = copy(arr);
+        helper(root.left, sum - root.val,arr1, ans);
+        helper(root.right, sum - root.val,arr2, ans);
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<Integer> list = new ArrayList();
+        List<List<Integer>> ans = new ArrayList();
+        helper(root, targetSum, list, ans);
+        return ans;
+    }
+}
 
