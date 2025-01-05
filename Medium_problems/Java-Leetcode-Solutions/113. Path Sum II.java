df1e689b5bca0b3a36312136789b6496b0b22181
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-05 20:41:21 +0530

    Update 113. Path Sum II.java

--- Medium_problems/Java-Leetcode-Solutions/113. Path Sum II.java
+++ Medium_problems/Java-Leetcode-Solutions/113. Path Sum II.java
@@ -1,4 +1,27 @@
-//Code using shallow copy
+//Optimised Code
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        List<Integer> list = new ArrayList();
+        List<List<Integer>> ans = new ArrayList();
+        helper(root, targetSum, list, ans);
+        return ans;
+    }
+
+    private static void helper(TreeNode root, int sum, List<Integer> list, List<List<Integer>> ans) {
+        if (root == null) return;
+        list.add(root.val);
+        if (root.val == sum && root.left == null && root.right == null)
+            ans.add(new ArrayList<Integer>(list));
+        else {
+            helper(root.left, sum - root.val, list, ans);
+            helper(root.right, sum - root.val, list, ans);
+        }
+        // remove the current node from the path to backtrack, 
+        // we need to remove the current node while we are going up the recursive call stack.
+        list.remove(list.size() - 1);
+    }
+}
+//slower method
 class Solution {
     public static List<Integer> copy(List<Integer> arr) {
         List<Integer> list = new ArrayList<>();
