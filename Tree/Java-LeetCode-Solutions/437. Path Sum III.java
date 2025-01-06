66c31258188c15cfb22a16502a547ac025afe318
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-06 21:52:25 +0530

    Update 437. Path Sum III.java

--- Tree/Java-LeetCode-Solutions/437. Path Sum III.java
+++ Tree/Java-LeetCode-Solutions/437. Path Sum III.java
@@ -1,1 +1,14 @@
+class Solution {
+    public static int helper(TreeNode root, long sum) {
+        if (root == null) return 0;
+        int paths = 0;
+        if (root.val == sum) paths++;
+        paths+=helper(root.left,sum - root.val) + helper(root.right,sum- root.val);
+        return paths; 
+    }
 
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root==null) return 0;
+        return helper(root, targetSum)+pathSum(root.left, targetSum)+pathSum(root.right, targetSum);
+    }
+}
