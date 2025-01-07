fd532fd43be9eaeefcc571f5f59f06f94d080d3e
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-07 23:14:41 +0530

    code added

--- Easy_problems/Java-Leetcode-Solutions/3174. Clear Digits.java
+++ Easy_problems/Java-Leetcode-Solutions/3174. Clear Digits.java
@@ -1,1 +1,20 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<String> st=new Stack<>();
+        char[] str = new char[s.length()];
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            if (s.charAt(i) > 96 && s.charAt(i) < 123){
+                st.push(s.charAt(i));   
+            }
+            else{
+                st.pop(s.charAt(i));
 
+            }
+        }
+        while(!st.isEmpty()){
+            str[j]=st.pop();
+        }
+        Collections.reverse(str);
+    }
+}
