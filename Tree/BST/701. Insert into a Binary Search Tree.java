5b193a605176f854454546134892aac6ba48d9f0
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-10 14:22:09 +0530

    Update 701. Insert into a Binary Search Tree.java

--- Tree/BST/701. Insert into a Binary Search Tree.java
+++ Tree/BST/701. Insert into a Binary Search Tree.java
@@ -1,1 +1,19 @@
+class Solution {
+    public TreeNode helper(TreeNode root, int val,TreeNode node) {
+        if (root == null)
+            return node;
+        if (root.val < val) {
+            root.right = helper(root.right, val,node);
+        } else {
+            root.left = helper(root.left, val,node);
+        }
+        return root;
+    }
 
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode node=new TreeNode(val);
+         if (root == null)
+            return node;
+        return helper(root, val,node);
+    }
+}
