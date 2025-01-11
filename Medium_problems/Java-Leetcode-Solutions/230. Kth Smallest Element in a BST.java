6bede818d5ebc9df63b17020df339466c9d3b814
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-11 21:57:36 +0530

    Update 230. Kth Smallest Element in a BST.java

--- Medium_problems/Java-Leetcode-Solutions/230. Kth Smallest Element in a BST.java
+++ Medium_problems/Java-Leetcode-Solutions/230. Kth Smallest Element in a BST.java
@@ -1,1 +1,24 @@
+public class Solution {
+    static int count;
+    static int result;
 
+    public void inorder(TreeNode node, int k) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, k);
+        count++;
+        if (count == k) {
+            result = node.val;
+            return;
+        }
+        inorder(node.right, k);
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        count = 0;
+        result = 0;
+        inorder(root, k);
+        return result;
+    }
+}
