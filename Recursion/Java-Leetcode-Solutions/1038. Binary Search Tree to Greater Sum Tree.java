099736dada455d094a6597c4dbffa500ff69ace3
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-15 21:08:03 +0530

    Update 1038. Binary Search Tree to Greater Sum Tree.java

--- Recursion/Java-Leetcode-Solutions/1038. Binary Search Tree to Greater Sum Tree.java
+++ Recursion/Java-Leetcode-Solutions/1038. Binary Search Tree to Greater Sum Tree.java
@@ -1,1 +1,17 @@
+class Solution {
+    static int sum=0;
+    public static void reverseInorder(TreeNode root) {
+        if (root == null)
+            return;
+        reverseInorder(root.right);
+        root.val+=sum;
+        sum=root.val;
+        reverseInorder(root.left);
+    }
 
+    public TreeNode bstToGst(TreeNode root) {
+        sum = 0;
+        reverseInorder(root);
+        return root;
+    }
+}
