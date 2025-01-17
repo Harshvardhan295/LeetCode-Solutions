6eeee2d23fc34ebfb08aa99349fffce850192b30
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-17 20:33:16 +0530

    Update 1382. Balance a Binary Search Tree.java

--- Recursion/Java-Leetcode-Solutions/1382. Balance a Binary Search Tree.java
+++ Recursion/Java-Leetcode-Solutions/1382. Balance a Binary Search Tree.java
@@ -1,1 +1,25 @@
 
+class Solution {
+    public static void inorder(TreeNode root, List<TreeNode> arr) {
+        if (root == null)
+            return;
+        inorder(root.left,arr);
+        arr.add(root);
+        inorder(root.right,arr);
+    }
+    public TreeNode helper(List<TreeNode> nums, int lo, int hi) {
+        if (lo > hi)
+            return null;
+        int midIdx = (lo+hi) / 2;
+        TreeNode root = new TreeNode(nums.get(midIdx).val);
+        root.left = helper(nums, lo, midIdx - 1);
+        root.right = helper(nums, midIdx + 1, hi);
+        return root;
+    }
+
+    public TreeNode balanceBST(TreeNode root) {
+        List<TreeNode> arr = new ArrayList<>();
+        inorder(root, arr);
+        return helper(arr, 0, arr.size() - 1);
+    }
+}
