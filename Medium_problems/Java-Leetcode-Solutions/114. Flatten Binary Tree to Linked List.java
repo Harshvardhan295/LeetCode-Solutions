0bdcd38f8b94ea24713d60cbf44050c2c0e07fd9
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-20 22:48:03 +0530

    Update 114. Flatten Binary Tree to Linked List.java

--- Medium_problems/Java-Leetcode-Solutions/114. Flatten Binary Tree to Linked List.java
+++ Medium_problems/Java-Leetcode-Solutions/114. Flatten Binary Tree to Linked List.java
@@ -1,3 +1,4 @@
+//Method 1: using arraylist
 class Solution {
     public static void preorder(TreeNode root, List<TreeNode> arr) {
         if (root == null)
@@ -18,6 +19,41 @@ class Solution {
             root=root.right;
         }    
         root.right=null;
-        
+    }
+}
+//Method 2 : recursive
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root==null) return;
+        if(root.left==null && root.right==null) return;
+        TreeNode lst=root.left;
+        TreeNode rst=root.right;
+        flatten(lst);
+        flatten(rst);
+        root.left=null;
+        root.right=lst;
+        TreeNode temp=root;
+        while(temp.right!=null) temp=temp.right;
+        temp.right=rst;
+    }
+}
+// Method 3 : Morris Traversal
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode curr=root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode r=curr.right;
+                curr.right=curr.left;
+                TreeNode pred=curr.left;
+                while(pred.right!=null) pred=pred.right;
+                pred.right=r;
+                curr.left=null;//Important
+                curr=curr.right;
+            }
+            else{
+                curr=curr.right;
+            }
+        }
     }
 }
