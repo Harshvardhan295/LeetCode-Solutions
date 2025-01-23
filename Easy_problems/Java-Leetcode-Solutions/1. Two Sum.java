9933ed20b2e8af02878789bf85c8c99261fe43bc
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-23 22:18:27 +0530

    Update 1. Two Sum.java

--- Easy_problems/Java-Leetcode-Solutions/1. Two Sum.java
+++ Easy_problems/Java-Leetcode-Solutions/1. Two Sum.java
@@ -1,3 +1,4 @@
+//Brute force
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
@@ -10,3 +11,21 @@ class Solution {
         return new int[]{};
     }
 
+// using hashmaps
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans={-1,-1};
+        HashMap<Integer,Integer> m1=new HashMap<>();
+        for(int i=0;i< nums.length;i++){
+            int rem=target-nums[i];
+            if(m1.containsKey(rem)){
+                int j=m1.get(rem);//returns value, index here
+                ans[0]=i;
+                ans[1]=j;
+                break;
+            }
+            else m1.put(nums[i],i);
+        }
+        return ans;
+    }
+}
