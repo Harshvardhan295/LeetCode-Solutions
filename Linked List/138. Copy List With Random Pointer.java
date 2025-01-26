839a5afb3c8da59d2ae8d31926ec49735bd5bafb
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-27 01:32:38 +0530

    Update 138. Copy List With Random Pointer.java

--- Linked List/138. Copy List With Random Pointer.java
+++ Linked List/138. Copy List With Random Pointer.java
@@ -1,18 +1,4 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
-*/
-
+//Optimise solution
 class Solution {
     public Node deepCopy(Node head){
          Node head2=new Node(head.val);
@@ -66,6 +52,36 @@ class Solution {
         connectAlternatively(head,head2);
         assignRandom(head,head2);
         split(head,head2);
+        return head2;
+    }
+}
+//using hashmap
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null)
+            return null;
+        Node head2 = new Node(head.val);
+        Node t2 = head2;
+        Node t1 = head.next;
+        while (t1 != null) {// creating deep copy
+            Node dup = new Node(t1.val);
+            t2.next = dup;
+            t1 = t1.next;
+            t2 = t2.next;
+        }
+        HashMap<Node, Node> map = new HashMap<>();
+        t2 = head2;
+        t1 = head;
+        while (t1 != null) {
+            map.put(t1, t2);
+            t1 = t1.next;
+            t2 = t2.next;
+        }
+        for (Node original : map.keySet()) {
+            Node duplicate = map.get(original);
+            if (original.random != null)
+                duplicate.random = map.get(original.random);
+        }
         return head2;
     }
 }
