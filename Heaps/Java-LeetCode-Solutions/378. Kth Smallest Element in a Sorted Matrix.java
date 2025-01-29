87c83036224d6584dd7a575e299a26329951fc37
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-30 00:06:47 +0530

    Update 378. Kth Smallest Element in a Sorted Matrix.java

--- Heaps/Java-LeetCode-Solutions/378. Kth Smallest Element in a Sorted Matrix.java
+++ Heaps/Java-LeetCode-Solutions/378. Kth Smallest Element in a Sorted Matrix.java
@@ -1,1 +1,39 @@
+//max heap
+import java.util.PriorityQueue;
 
+class Solution {
+    public int kthSmallest(int[][] a, int k) {
+        int m = a.length;
+        int n = a[0].length;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());//max heap
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pq.add(a[i][j]);
+                if (pq.size() > k) {
+                    pq.remove();
+                }
+            }
+        }
+        return pq.peek();
+    }
+}
+//min heap
+class Solution {
+    public int kthSmallest(int[][] a, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int m = a.length;
+        int n = a[0].length;
+        // Min Heap
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                pq.add(a[i][j]);
+            }
+        }
+        int i = 0;
+        while (i < k - 1) {
+            pq.remove();
+            i++;
+        }
+        return pq.peek();
+    }
+}
