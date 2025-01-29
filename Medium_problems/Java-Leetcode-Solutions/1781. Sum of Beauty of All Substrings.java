21c3369029224ec5e7c51232d65d71a1cbb29c82
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-01-29 23:22:32 +0530

    Create 1781. Sum of Beauty of All Substrings.java

---
+++ Medium_problems/Java-Leetcode-Solutions/1781. Sum of Beauty of All Substrings.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+class Solution {
+    public int beautySum(String s) {
+        int sum = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (int j = i; j < n; j++) {
+                char ch = s.charAt(j);
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+                int maxfreq = Integer.MIN_VALUE;
+                int minfreq = Integer.MAX_VALUE;
+                
+                for (int freq : map.values()) {
+                    maxfreq = Math.max(maxfreq, freq);
+                    minfreq = Math.min(minfreq, freq);
+                }
+                sum += (maxfreq - minfreq); 
+            }
+        }
+        return sum;
+    }
+}
