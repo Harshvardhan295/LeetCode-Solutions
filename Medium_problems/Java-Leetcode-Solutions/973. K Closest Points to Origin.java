3274015b1b4c7d7cc2ee4ffa29becee9195c9286
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-01 22:11:05 +0530

    Update 973. K Closest Points to Origin.java

--- Medium_problems/Java-Leetcode-Solutions/973. K Closest Points to Origin.java
+++ Medium_problems/Java-Leetcode-Solutions/973. K Closest Points to Origin.java
@@ -1,3 +1,4 @@
+//method 1
 class Solution {
     public class Pair {
         int x, y;
@@ -31,6 +32,41 @@ class Solution {
             ans[i][1] = point.y;
         }
 
+        return ans;
+    }
+}
+//method 2
+class Solution {
+    public class Triplet implements Comparable<Triplet>{
+        int x, y,d;
+        Triplet(int d,int x, int y) {
+            this.x = x;
+            this.y = y;
+            this.d=d;
+        }
+        public int compareTo(Triplet t){
+            return this.d-t.d;
+        }
+    }
+
+    public int[][] kClosest(int[][] a, int k) {
+        int m = a.length;
+        PriorityQueue<Triplet> pq = new PriorityQueue<>(Collections.reverseOrder()); // max heap
+        for (int i = 0; i < m; i++) {
+            int x=a[i][0],y=a[i][1];
+            int d = x*x+y*y; // distance formula from origin
+            pq.add(new Triplet(d,x,y));
+            if (pq.size() > k) pq.remove();// removing greater distance
+
+        }
+
+        int[][] ans = new int[k][2];
+        for (int i = 0; i < k; i++) {
+            // req. closest distance
+            Triplet t = pq.remove();
+            ans[i][0] = t.x;
+            ans[i][1] = t.y;
+        }
         return ans;
     }
 }
