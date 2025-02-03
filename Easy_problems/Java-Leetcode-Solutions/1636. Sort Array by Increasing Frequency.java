abae69d46687bdf3d5417bac08db425d5dc8577e
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-03 23:08:52 +0530

    Update 1636. Sort Array by Increasing Frequency.java

--- Easy_problems/Java-Leetcode-Solutions/1636. Sort Array by Increasing Frequency.java
+++ Easy_problems/Java-Leetcode-Solutions/1636. Sort Array by Increasing Frequency.java
@@ -1,1 +1,38 @@
+class Solution {
+    class Pair implements Comparable<Pair>{
+        int x;
+        int freq;
+        Pair(int x,int freq){
+            this.x=x;
+            this.freq=freq;
+        }
+        public int compareTo(Pair p){
+            if (this.freq == p.freq) {
+                return p.x - this.x; //when frequency is the same,sort by value in descending order 
+            }
+            return this.freq-p.freq;
+        }
+    }
+    public int[] frequencySort(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        PriorityQueue<Pair> pq=new PriorityQueue<>();
 
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        //Min Heap
+        for(int ele:map.keySet()){
+            int freq=map.get(ele);
+            pq.add(new Pair(ele,freq));
+        }
+        int i = 0;
+        while (!pq.isEmpty()) {
+            Pair pair = pq.remove();
+            int count = pair.freq;
+            while (count-- > 0) {
+                nums[i++] = pair.x;
+            }
+        }
+        return nums;
+    }
+}
