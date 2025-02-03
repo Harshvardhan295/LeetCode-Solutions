b29902005b60c4d0a0f6e68320adb33ea8c2a474
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-03 23:11:35 +0530

    Update 347. Top K Frequent Elements.java

--- Medium_problems/Java-Leetcode-Solutions/347. Top K Frequent Elements.java
+++ Medium_problems/Java-Leetcode-Solutions/347. Top K Frequent Elements.java
@@ -1,1 +1,35 @@
+class Solution {
+    class Pair implements Comparable<Pair>{
+        int x;
+        int freq;
+        Pair(int x,int freq){
+            this.x=x;
+            this.freq=freq;
+        }
+        public int compareTo(Pair p){
+            return this.freq-p.freq;
+        }
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        PriorityQueue<Pair> pq=new PriorityQueue<>();
 
+        for(int i=0;i< nums.length;i++) {
+            int ele = nums[i];
+            map.put( ele, map.getOrDefault(ele, 0) + 1);
+        }
+        //Min Heap
+        for(int ele:map.keySet()){
+            int freq=map.get(ele);
+            pq.add(new Pair(ele,freq));
+            if(pq.size()>k) pq.remove();
+        }
+
+        int[] ans=new int[k];
+        for(int i=0;i<k;i++){
+            Pair pair=pq.remove();
+            ans[i]=pair.x;
+        }
+        return ans;
+    }
+}
