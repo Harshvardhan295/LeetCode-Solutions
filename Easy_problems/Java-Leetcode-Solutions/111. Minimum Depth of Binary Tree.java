e86ece126c84df354b6ac1f4019555bfaebc0744
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-12 16:29:43 +0530

    Update 111. Minimum Depth of Binary Tree.java

--- Easy_problems/Java-Leetcode-Solutions/111. Minimum Depth of Binary Tree.java
+++ Easy_problems/Java-Leetcode-Solutions/111. Minimum Depth of Binary Tree.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null)  return 0;
 
+        int lDepth = minDepth(root.left);
+        int rDepth = minDepth(root.right);
+        if(root.left == null && root.right == null)
+            return 1;
+
+        if(root.left == null)
+            return 1 + rDepth;
+        if(root.right == null)
+            return 1 + lDepth;
+
+        return Math.min(lDepth, rDepth) + 1;
+    }
+}
