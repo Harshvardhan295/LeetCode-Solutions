5029550646440d9ffd801dc81665a91e80b97e89
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-13 20:54:42 +0530

    Update 268. Missing Number.java

--- Easy_problems/Java-Leetcode-Solutions/268. Missing Number.java
+++ Easy_problems/Java-Leetcode-Solutions/268. Missing Number.java
@@ -13,3 +13,23 @@ class Solution {
     }
 }
 
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i <= n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+        
