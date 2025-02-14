00d79dc2ba2d3115720cba6c79cd29b8e1b567b0
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-14 23:47:07 +0530

    Added code using hashmaps

--- Easy_problems/Java-Leetcode-Solutions/169. Majority Element.java
+++ Easy_problems/Java-Leetcode-Solutions/169. Majority Element.java
@@ -1,1 +1,20 @@
+//Using Hashmaps
+class Solution {
+    public int majorityElement(int[] a) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int ele : a) {
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+        
+        int ans = -1, max = 0;
+        for (int ele : map.keySet()) {
+            if (map.get(ele) > max) {
+                max = map.get(ele);
+                ans = ele;
+            }
+        }
+        return ans; 
+    }
+}
 
