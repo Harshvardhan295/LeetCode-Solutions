533b863b7d5f3b8bd8fb24311927c3e757b19c12
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-14 23:48:24 +0530

    Update 169. Majority Element.java

--- Easy_problems/Java-Leetcode-Solutions/169. Majority Element.java
+++ Easy_problems/Java-Leetcode-Solutions/169. Majority Element.java
@@ -17,4 +17,11 @@ class Solution {
         return ans; 
     }
 }
-
+//O(nlogn)
+class Solution {
+    public int majorityElement(int[] a) {
+        int n=a.length;
+        Arrays.sort(a);
+        return a[n/2];
+    }
+}
