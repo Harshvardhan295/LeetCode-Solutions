5e1d356818b88575a3d306def1b684c96015f87c
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-15 22:39:47 +0530

    Update 2124. Check if All A's Appears Before All B's.java

--- Easy_problems/Java-Leetcode-Solutions/2124. Check if All A's Appears Before All B's.java
+++ Easy_problems/Java-Leetcode-Solutions/2124. Check if All A's Appears Before All B's.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public boolean checkString(String s) {
+        boolean flag=true;
+        int countb=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='b'){
+                countb++;
+            }
+            else{
+                if(countb>0) return false;
+            }
+        }
+        return true;
+    }
+}
