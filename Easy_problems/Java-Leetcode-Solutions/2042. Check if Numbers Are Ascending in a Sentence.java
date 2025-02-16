f6693c3688b180f6513e2f778259d86751808b80
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-16 21:52:39 +0530

    Create 2042. Check if Numbers Are Ascending in a Sentence.java

---
+++ Easy_problems/Java-Leetcode-Solutions/2042. Check if Numbers Are Ascending in a Sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean areNumbersAscending(String s) {
+        int prev = 0;
+        String[] words = s.split(" ");//bresks sentences into words
+
+        for (String word : words) {
+            if (Character.isDigit(word.charAt(0))) { 
+                int num = Integer.parseInt(word); 
+                if (num <= prev) return false; 
+                prev = num;
+            }
+        }
+        return true;
+    }
+}
