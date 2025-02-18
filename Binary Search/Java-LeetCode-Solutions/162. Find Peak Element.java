6e586064fd853042fa18861f11ef0045fbbaa3c8
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-18 21:35:21 +0530

    Update 162. Find Peak Element.java

--- Binary Search/Java-LeetCode-Solutions/162. Find Peak Element.java
+++ Binary Search/Java-LeetCode-Solutions/162. Find Peak Element.java
@@ -15,3 +15,19 @@ class Solution {
         return low;
     }
 }
+//Brute Force
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return 0; //if length is 1
+        if (arr[0] > arr[1]) return 0; // for 1st element
+        if (arr[n - 1] > arr[n - 2]) return n - 1; // for last element
+
+        for (int i = 1; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1] && arr[i] > arr[i - 1]) {
+                return i;
+            }
+        }
+        return 0; 
+    }
+}
