b3597371a9868f6850e1466448959b2ed4536dcb
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-21 23:54:43 +0530

    Update 295. Find Median from Data Stream.java

--- Hard_problems/Java-LeetCode-Solutions/295. Find Median from Data Stream.java
+++ Hard_problems/Java-LeetCode-Solutions/295. Find Median from Data Stream.java
@@ -16,3 +16,31 @@ List<Integer> arr=new ArrayList<>();
     else return (arr.get(n/2)+ arr.get(n/2-1))/2.0;
     }
 }
+class MedianFinder {
+List<Integer> arr=new ArrayList<>();
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+        arr.add(num);
+        int i=arr.size()-1;
+        while(i>0){
+            if(arr.get(i)<arr.get(i-1)){
+                swap(i,i-1);
+                i--;
+            }
+            else break;
+        }
+    }
+    public void swap(int i,int j){
+        int temp=arr.get(i);
+        arr.set(i,arr.get(j));
+        arr.set(j,arr.get(temp));
+    }
+    public double findMedian() {
+    int n=arr.size();
+    if(n%2!=0) return arr.get(n/2); //odd length
+    else return (arr.get(n/2)+ arr.get(n/2-1))/2.0;
+    }
+}
