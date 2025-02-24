2463ff01096271af95e81ad25676c08b2ca587a3
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-02-24 21:21:54 +0530

    Update 1480. Running Sum of 1d Array.java

--- Easy_problems/Java-Leetcode-Solutions/1480. Running Sum of 1d Array.java
+++ Easy_problems/Java-Leetcode-Solutions/1480. Running Sum of 1d Array.java
@@ -1,1 +1,9 @@
-
+//prefix sum
+class Solution {
+    public int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++){
+            nums[i]=nums[i]+nums[i-1];
+        }
+        return nums;
+    }
+}
