461aa6c9494328dc49c3a93b141d77064a0e3f97
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-01 22:01:42 +0530

    Update 724. Find Pivot Index.java

--- Easy_problems/Java-Leetcode-Solutions/724. Find Pivot Index.java
+++ Easy_problems/Java-Leetcode-Solutions/724. Find Pivot Index.java
@@ -15,3 +15,19 @@ class Solution {
         return -1;
     }
 }
+//Prefix Sum
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            nums[i]+=nums[i-1];
+        }
+        for(int i=0;i<n;i++){
+            int leftSum=0;
+            if(i>0) leftSum=nums[i-1];
+            int rightSum=nums[n-1]-nums[i];
+            if(leftSum==rightSum) return i;
+        }
+        return -1;
+    }
+}
