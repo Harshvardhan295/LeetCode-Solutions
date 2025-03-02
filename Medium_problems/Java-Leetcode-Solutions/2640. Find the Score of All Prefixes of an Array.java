0b3c29dce20f5d74b6a13b71a27dac87d184aa09
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-02 22:02:38 +0530

    Update 2640. Find the Score of All Prefixes of an Array.java

--- Medium_problems/Java-Leetcode-Solutions/2640. Find the Score of All Prefixes of an Array.java
+++ Medium_problems/Java-Leetcode-Solutions/2640. Find the Score of All Prefixes of an Array.java
@@ -1,1 +1,14 @@
+class Solution {
+    public long[] findPrefixScore(int[] nums) {
+        long[] convert = new long[nums.length];
+        long max = 0, sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max, nums[i]);  
+            sum += nums[i] + max;          
+            convert[i] = sum;              
+        }
+        return convert;
+    }
+}
 
