73f95cba7bbb05ffc69a1cc11ff269ffece571e3
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-03 23:37:37 +0530

    Update 238. Product of Array Except Self.java

--- Medium_problems/Java-Leetcode-Solutions/238. Product of Array Except Self.java
+++ Medium_problems/Java-Leetcode-Solutions/238. Product of Array Except Self.java
@@ -1,3 +1,4 @@
+//Optimised code
 class Solution {
     public int[] productExceptSelf(int[] arr) {
         int n = arr.length;
@@ -13,6 +14,31 @@ class Solution {
             prefix *= arr[i];
         }
         
+        
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= arr[i];
+        }
+        
+        return result;
+    }
+}
+//Space complexity:O(n)
+class Solution {
+    public int[] productExceptSelf(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            result[i] = 1;
+        }
+        
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix;
+            prefix *= arr[i];
+        }
         
         int suffix = 1;
         for (int i = n - 1; i >= 0; i--) {
