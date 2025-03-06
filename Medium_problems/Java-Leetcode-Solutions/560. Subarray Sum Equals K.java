abae6f00ff43ba2ddd39d5e2394ffd3c341a1781
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-06 22:41:39 +0530

    Update 560. Subarray Sum Equals K.java

--- Medium_problems/Java-Leetcode-Solutions/560. Subarray Sum Equals K.java
+++ Medium_problems/Java-Leetcode-Solutions/560. Subarray Sum Equals K.java
@@ -1,1 +1,17 @@
-
+//Prefix Sum and Hashmaps
+class Solution {
+     public int subarraySum(int[] arr, int k) {
+        int count=0,n=arr.length;
+        for(int i=1;i<n;i++){ //prefix sum
+            arr[i]+=arr[i-1];
+        }
+        HashMap<Integer,Integer> maps=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(arr[i]==k) count++;
+            int rem=arr[i]-k;
+            if(maps.containsKey(rem) count+=maps.get(rem);
+            maps.put(arr[i],maps.getOrDefault(arr[i],0)+1);
+        }
+        return count;
+    }
+}
