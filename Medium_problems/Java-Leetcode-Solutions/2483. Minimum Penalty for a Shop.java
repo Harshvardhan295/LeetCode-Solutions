4e59958c06b16a1389a7b3f5f436736fc2e16c4c
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-06 23:52:55 +0530

    Update 2483. Minimum Penalty for a Shop.java

--- Medium_problems/Java-Leetcode-Solutions/2483. Minimum Penalty for a Shop.java
+++ Medium_problems/Java-Leetcode-Solutions/2483. Minimum Penalty for a Shop.java
@@ -1,1 +1,28 @@
-
+//PrefixSum
+class Solution {
+    public int bestClosingTime(String s) {
+        int n = s.length();
+        int[] preN = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            preN[i] = preN[i - 1];
+            if (s.charAt(i - 1) == 'N')
+                preN[i] += 1;
+        }
+        int[] sufY = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            sufY[i] = sufY[i + 1];
+            if (s.charAt(i) == 'Y')
+                sufY[i] += 1;
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i <= n; i++) {
+            preN[i] += sufY[i];
+            min = Math.min(preN[i], min);
+        }
+        for(int i=0;i<=n;i++){
+            if(preN[i]==min) return i;
+        }
+        return -1;
+    }
+}
