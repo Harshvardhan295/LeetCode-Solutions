4ffaad501a759d3bbf183eb2ab5f0b8a12b2c8ab
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-09 20:48:26 +0530

    Update 456. 132 Pattern.java

--- Medium_problems/Java-Leetcode-Solutions/456. 132 Pattern.java
+++ Medium_problems/Java-Leetcode-Solutions/456. 132 Pattern.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        if(nums.length<3){
+            return false;
+        }
+        int prevMin=Integer.MIN_VALUE;
+        int peak=nums.length;
+        for(int i=nums.length-1;i>=0;i--){
+            if(nums[i]<prevMin) return true;
+            while(peak<nums.length && nums[i] > nums[peak]){
+                prevMin=nums[peak++];
+            }
+            peak--;
+        nums[peak]=nums[i];
+        }
+        return false;
+    }
+}
