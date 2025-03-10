f722e4591a743aaafad54919ab14c5ecd9e1bdae
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-10 20:25:53 +0530

    Update 1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.java

--- Medium_problems/Java-Leetcode-Solutions/1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.java
+++ Medium_problems/Java-Leetcode-Solutions/1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        int n = arr.length;
+        //Sliding Window
+        int Sum=0,average=0, count=0;
+        for(int i=0;i<k;i++){
+            Sum+=arr[i];
+        }
+        average=Sum/k;
+        if(average>=threshold) count++;
 
+        for(int i=k;i<n;i++){
+            Sum+=arr[i]-arr[i-k];
+            average=Sum/k;
+            if(average>=threshold) count++;
+        }
+        return count;
+    }
+}
