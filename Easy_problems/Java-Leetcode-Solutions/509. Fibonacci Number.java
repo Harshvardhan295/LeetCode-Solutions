9bd86835f008ae6196276a1f608469418e150611
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-18 22:54:53 +0530

    Update 509. Fibonacci Number.java

--- Easy_problems/Java-Leetcode-Solutions/509. Fibonacci Number.java
+++ Easy_problems/Java-Leetcode-Solutions/509. Fibonacci Number.java
@@ -5,3 +5,18 @@ class Solution {
         else return fib(n-1)+fib(n-2);
     }
 }
+//DP
+class Solution {
+    public int helper(int n, int[] dp) {
+        if (n <= 1) return n;
+        if (dp[n] != 0) return dp[n];
+        int ans = helper(n - 1, dp) + helper(n - 2, dp);
+        dp[n] = ans;
+        return ans;
+    }
+
+    public int fib(int n) {
+        int[] dp = new int[n + 1]; //dp with memoization
+        return helper(n, dp);
+    }
+}
