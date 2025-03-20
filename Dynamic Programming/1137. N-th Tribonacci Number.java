36734bcfa0dd138f3ec9d100b4f31609e8b2c32a
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-20 23:06:11 +0530

    Update 1137. N-th Tribonacci Number.java

--- Dynamic Programming/1137. N-th Tribonacci Number.java
+++ Dynamic Programming/1137. N-th Tribonacci Number.java
@@ -6,3 +6,17 @@ class Solution {
         return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
     }
 }
+//Memoization
+class Solution {
+    public int helper(int n,int[] dp){
+        if(n==0) return 0;
+        if(n==1 ||n==2) return 1;
+        if(dp[n]!=0) return dp[n];
+        return dp[n]=helper(n-1,dp)+helper(n-2,dp)+helper(n-3,dp);
+    }
+    public int tribonacci(int n) {
+        int[] dp=new int[n+1];
+        return helper(n,dp);
+    }
+}
+//Tabulation
