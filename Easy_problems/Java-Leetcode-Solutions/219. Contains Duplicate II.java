19a4773b26661aee5545767986f4087dd983e85e
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-23 20:09:46 +0530

    Update 219. Contains Duplicate II.java

--- Easy_problems/Java-Leetcode-Solutions/219. Contains Duplicate II.java
+++ Easy_problems/Java-Leetcode-Solutions/219. Contains Duplicate II.java
@@ -11,3 +11,31 @@ class Solution {
 
     }
 }
+//Hashmap
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                int j=map.get(nums[i]);
+                if(i-j<=k) return true;
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
+//Hashset and Sliding window
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k)  set.remove(nums[i - k - 1]);
+            if (set.contains(nums[i])) return true; // Duplicate found within the window of size k
+            set.add(nums[i]);
+        }
+        return false; // No duplicate found within the given distance k
+    }
+}
