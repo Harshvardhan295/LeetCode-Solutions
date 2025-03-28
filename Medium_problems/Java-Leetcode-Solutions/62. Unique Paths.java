22fb7b514211861776fd71a62ed9207ba90abb03
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-28 20:14:43 +0530

    Create 62. Unique Paths.java

---
+++ Medium_problems/Java-Leetcode-Solutions/62. Unique Paths.java
@@ -0,0 +1,14 @@
+//Recursion
+class Solution {
+    public int ways(int row, int col, int m, int n) {
+        if (row == m - 1 && col == n - 1)
+            return 1;
+        if (row >= m || col >= n)
+            return 0;
+        return ways(row + 1, col, m, n) + ways(row, col + 1, m, n);
+    }
+
+    public int uniquePaths(int m, int n) {
+        return ways(0, 0, m, n); // both works
+    }
+}
