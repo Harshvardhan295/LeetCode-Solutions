a6e153db6e2bfbead7c5d9e4b8603958c1b21127
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-28 20:15:11 +0530

    Update 62. Unique Paths.java

--- Medium_problems/Java-Leetcode-Solutions/62. Unique Paths.java
+++ Medium_problems/Java-Leetcode-Solutions/62. Unique Paths.java
@@ -12,3 +12,24 @@ class Solution {
         return ways(0, 0, m, n); // both works
     }
 }
+//memoization
+class Solution {
+    public int ways(int row, int col, int m, int n,int[][] dp) {
+        if (row == m - 1 && col == n - 1)
+            return 1;
+        if (row >= m || col >= n)
+            return 0;
+        if(dp[row][col]!=-1) return dp[row][col];
+        return dp[row][col]=ways(row + 1, col, m, n,dp) + ways(row, col + 1, m, n,dp);
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return ways(0, 0, m, n,dp); // both works
+    }
+}
