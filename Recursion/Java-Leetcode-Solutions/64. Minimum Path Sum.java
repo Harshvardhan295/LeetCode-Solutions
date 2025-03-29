620ef382b01d317610fd0920d6ea6c2ef65aed7e
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-29 20:59:56 +0530

    Update 64. Minimum Path Sum.java

--- Recursion/Java-Leetcode-Solutions/64. Minimum Path Sum.java
+++ Recursion/Java-Leetcode-Solutions/64. Minimum Path Sum.java
@@ -14,3 +14,43 @@ class Solution {
         return path(grid, 0, 0, m, n);
     }
 }
+//Memoization
+class Solution {
+    public int path(int[][] grid, int i, int j, int m, int n, int[][] dp) {
+        if (i >= m || j >= n) return Integer.MAX_VALUE; 
+        if (i == m - 1 && j == n - 1) return grid[i][j]; 
+        if (dp[i][j] != -1) return dp[i][j]; 
+        
+        int right = path(grid, i, j + 1, m, n, dp); 
+        int down = path(grid, i + 1, j, m, n, dp);  
+
+        return dp[i][j] = grid[i][j] + Math.min(right, down);
+    }
+
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1); 
+        }
+        return path(grid, 0, 0, m, n, dp);
+    }
+}
+//Tabulation
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) grid[i][j] = grid[i][j];
+                else if (i == 0) grid[i][j] += grid[i][j - 1];
+                else if (j == 0) grid[i][j] += grid[i - 1][j];
+                else grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
+            }
+        }
+        return grid[m - 1][n - 1];
+    }
+}
