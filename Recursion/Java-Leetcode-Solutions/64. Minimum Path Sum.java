3b3e74cbf7fa5f38c4aec1980e8a5640e6b9ecdf
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-29 20:58:51 +0530

    Update 64. Minimum Path Sum.java

--- Recursion/Java-Leetcode-Solutions/64. Minimum Path Sum.java
+++ Recursion/Java-Leetcode-Solutions/64. Minimum Path Sum.java
@@ -1,1 +1,16 @@
+//Recursion
+class Solution {
+    public int path(int[][] grid, int i, int j, int m, int n) {
+        if (i >= m || j >= n) return Integer.MAX_VALUE; 
+        if (i == m - 1 && j == n - 1) return grid[i][j]; 
+        int right = path(grid, i, j + 1, m, n); 
+        int down = path(grid, i + 1, j, m, n);  
+        return grid[i][j] + Math.min(right, down);
+    }
 
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        return path(grid, 0, 0, m, n);
+    }
+}
