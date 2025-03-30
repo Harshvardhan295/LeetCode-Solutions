6b2b27fb422c552df2dd876ed3381b9f19d92425
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-03-30 16:30:18 +0530

    Update 3370. Smallest Number With All Set Bits.java

--- Easy_problems/Java-Leetcode-Solutions/3370. Smallest Number With All Set Bits.java
+++ Easy_problems/Java-Leetcode-Solutions/3370. Smallest Number With All Set Bits.java
@@ -1,1 +1,20 @@
-
+//-------M1-------
+  class Solution {
+    public int smallestNumber(int n) {
+        int res = 1;
+        while (res < n)
+            res = res * 2 + 1;
+        return res;
+    }
+}
+//-------M2--------
+class Solution {
+    public int smallestNumber(int n) {
+        int power=1;
+        for(int i=0;i<n;i++){
+            power*=2;
+            if(power>n) return power-1;
+        }
+        return power-1;
+    }
+}
