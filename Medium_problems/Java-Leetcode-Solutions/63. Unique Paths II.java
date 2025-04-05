3d8660060be2232c8bc9bc0a89bf75e8967e726a
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-04-05 20:48:13 +0530

    Update 63. Unique Paths II.java

--- Medium_problems/Java-Leetcode-Solutions/63. Unique Paths II.java
+++ Medium_problems/Java-Leetcode-Solutions/63. Unique Paths II.java
@@ -1,1 +1,29 @@
+//Recursion
+class Solution {
+    private int uniquePathsWithObstacles(int[][] arr, int row, int col) {
+        int m = arr.length;
+        int n = arr[0].length;
 
+        if (arr[0][0] == 1 || arr[m - 1][n - 1] == 1) {
+            return 0;
+        }
+
+        if (row == m - 1 && col == n - 1) {
+            return 1;
+        }
+
+        if (row >= m || col >= n || arr[row][col] == 1) {
+            return 0;
+        }
+
+        int rightWays = uniquePathsWithObstacles(arr, row, col + 1);
+        int downWays = uniquePathsWithObstacles(arr, row + 1, col);
+
+        return rightWays + downWays;
+    }
+    public int uniquePathsWithObstacles(int[][] arr) {
+        return uniquePathsWithObstacles(arr, 0, 0); //same method name but with different arguments
+    }
+
+    
+}
