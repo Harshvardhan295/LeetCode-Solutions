979cec67d3736ebedf3552bbc266490dac35f54e
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-04-05 20:59:47 +0530

    Update 63. Unique Paths II.java

--- Medium_problems/Java-Leetcode-Solutions/63. Unique Paths II.java
+++ Medium_problems/Java-Leetcode-Solutions/63. Unique Paths II.java
@@ -53,3 +53,32 @@ class Solution {
         return uniquePathsWithObstacles(arr, 0, 0,dp); //same method name but with different arguments
     }   
 }
+//Tabulation
+class Solution {
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        if (arr[0][0] == 1 || arr[m - 1][n - 1] == 1) return 0;//start and goal is obstacle
+
+        arr[0][0] = 1;
+
+        //Convert the first row and first column to 0 if obstacle and 1 if not
+        for (int i = 1; i < m; i++) {
+            if (arr[i][0] == 1) arr[i][0] = 0;//If obstacle, set it to 0
+            else arr[i][0] = arr[i-1][0];
+        }
+        for (int i = 1; i < n; i++) {
+            if (arr[0][i] == 1) arr[0][i] = 0;//If obstacle, set it to 0
+            else arr[0][i] = arr[0][i-1];
+        }
+
+        //for rest of the matrix
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == 1) arr[i][j] = 0;//If obstacle, set it to 0
+                else arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+            }
+        }
+        return arr[m - 1][n - 1];
+    }
+}
