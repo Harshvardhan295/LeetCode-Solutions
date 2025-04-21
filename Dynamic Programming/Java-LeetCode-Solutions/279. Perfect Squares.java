c4d9edc0b247ba877e8df0c4ed7b07acde3553fa
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-04-21 22:43:46 +0530

    Update 279. Perfect Squares.java

--- Dynamic Programming/Java-LeetCode-Solutions/279. Perfect Squares.java
+++ Dynamic Programming/Java-LeetCode-Solutions/279. Perfect Squares.java
@@ -1,1 +1,18 @@
+//recursion
+class Solution {
+    public boolean isPerfect(int n) {
+    int sqrt = (int) Math.sqrt(n);
+    return sqrt * sqrt == n;
+}
 
+    public int numSquares(int n) {
+        if(isPerfect(n)) return 1;
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i<=n/2;i++){
+            int count=numSquares(i)+numSquares(n-i);
+            min=Math.min(min,count);
+        }
+        return min;
+    }
+}
+//
