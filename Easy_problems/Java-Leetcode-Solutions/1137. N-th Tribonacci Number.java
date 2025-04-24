0af0d3a0e7a8f1b4a575033819fd806c06b4ecae
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-04-24 23:49:12 +0530

    Update 1137. N-th Tribonacci Number.java

--- Easy_problems/Java-Leetcode-Solutions/1137. N-th Tribonacci Number.java
+++ Easy_problems/Java-Leetcode-Solutions/1137. N-th Tribonacci Number.java
@@ -1,4 +1,22 @@
-//Recursive
+//Space Optimisation
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1 ||n==2) return 1;
+        int[] dp=new int[4]; //store results for all indices from 0 to n
+        dp[0]= 0;
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[3]= dp[2]+dp[1]+dp[0];
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+            dp[2]=dp[3];
+        }
+        return dp[3];
+    }
+}
+//recursion
 class Solution {
     public int tribonacci(int n) {
         if(n==0) return 0;
