7807115aa8ae0eb5655fe2d8376fe40438396574
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-04-28 22:29:04 +0530

    Update 349. Intersection of Two Arrays.java

--- Hash Table/HashSet/Java-LeetCode-Solutions/349. Intersection of Two Arrays.java
+++ Hash Table/HashSet/Java-LeetCode-Solutions/349. Intersection of Two Arrays.java
@@ -2,16 +2,16 @@ class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        Set<Integer> set = new HashSet<>();//for unique elements
+        Set<Integer> set = new HashSet<>(); // for unique elements
         
-        for (int j = 0; j < nums2.length; j++) {
+        for (int num : nums2) {
             int low = 0, high = nums1.length - 1;
             while (low <= high) {
                 int mid = low + (high - low) / 2;
-                if (nums1[mid] == nums2[j]) {
-                    set.add(nums2[j]);
+                if (nums1[mid] == num) {
+                    set.add(num);
                     break; // break binary search if found
-                } else if (nums1[mid] < nums2[j]) {
+                } else if (nums1[mid] < num) {
                     low = mid + 1;
                 } else {
                     high = mid - 1;
@@ -21,10 +21,9 @@ class Solution {
         
         int[] ans = new int[set.size()];
         int i = 0;
-        for (int ele : set) {
-            ans[i++] = ele;
+        for (int num : set) {
+            ans[i++] = num;
         }
         return ans;
     }
 }
-
