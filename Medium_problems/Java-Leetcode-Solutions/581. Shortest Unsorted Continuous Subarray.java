f7ceb50c490836eaa89486dfd6ccbe18880d50d6
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-05-14 23:28:09 +0530

    Update 581. Shortest Unsorted Continuous Subarray.java

--- Medium_problems/Java-Leetcode-Solutions/581. Shortest Unsorted Continuous Subarray.java
+++ Medium_problems/Java-Leetcode-Solutions/581. Shortest Unsorted Continuous Subarray.java
@@ -1,1 +1,36 @@
+//Method 1
+class Solution {
+    
+    public int findUnsortedSubarray(int[] nums) {
+        int n=nums.length;
+        int start=0,end=n-1;
+        // Find the first index where order breaks
+        while(start<n-1 && nums[start]<=nums[start+1]) start++;
 
+        // If order does not break then array is already sorted
+        if (start == n - 1) {
+            return 0;
+        }
+
+        // Find the last index where order breaks
+        while(end>0 && nums[end-1]<=nums[end]) end--;
+
+        //find the max and min in this sub array
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int i = start; i <= end; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+
+        //now check if elements of nums before start are smaller than min and after end are greater than max
+        while (start > 0 && nums[start - 1] > min) {
+            start--;
+        }
+
+        while (end < n - 1 && nums[end + 1] < max) {
+            end++;
+        }
+
+        return end-start+1;
+    }
+}
