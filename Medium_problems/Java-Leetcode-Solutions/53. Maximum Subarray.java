5743537bd7e7237bd4cc92274aede5b6de0fe90f
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-05-16 23:10:24 +0530

    Update 53. Maximum Subarray.java

--- Medium_problems/Java-Leetcode-Solutions/53. Maximum Subarray.java
+++ Medium_problems/Java-Leetcode-Solutions/53. Maximum Subarray.java
@@ -1,3 +1,41 @@
+//Divide and Conquer
+class Solution {
+    public static int maxSubArrayHelper(int[] nums, int left, int right) {
+        if (left == right) {
+            return nums[left];
+        }
+
+        int mid = (left + right) / 2;
+
+        int leftSum = maxSubArrayHelper(nums, left, mid);
+        int rightSum = maxSubArrayHelper(nums, mid + 1, right);
+        int crossSum = maxCrossingSum(nums, left, mid, right);
+
+        return Math.max(Math.max(leftSum, rightSum), crossSum);
+    }
+
+    public static int maxCrossingSum(int[] nums, int left, int mid, int right) {
+        int leftMaxSum = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = mid; i >= left; i--) {
+            sum += nums[i];
+            leftMaxSum = Math.max(leftMaxSum, sum);
+        }
+
+        int rightMaxSum = Integer.MIN_VALUE;
+        sum = 0;
+        for (int i = mid + 1; i <= right; i++) {
+            sum += nums[i];
+            rightMaxSum = Math.max(rightMaxSum, sum);
+        }
+
+        return leftMaxSum + rightMaxSum;
+    }
+    public int maxSubArray(int[] nums) {
+        return maxSubArrayHelper(nums,0,nums.length-1);
+    }
+}
+
 //-------------------Optimised code----------------
 class Solution {
     public int maxSubArray(int[] nums) {
