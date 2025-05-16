e1386a0e569c7f0ff3a88525c202e026c27f813d
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-05-16 23:16:55 +0530

    Update 1945. Sum of Digits of String After Convert.java

--- Easy_problems/Java-Leetcode-Solutions/1945. Sum of Digits of String After Convert.java
+++ Easy_problems/Java-Leetcode-Solutions/1945. Sum of Digits of String After Convert.java
@@ -1,1 +1,23 @@
+// Iterative approach
+class Solution {
+    public int getLucky(String str, int k) {
+        //Convert letters to their alphabet positions
+        StringBuilder num = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            int n = ch - 'a' + 1;
+            num.append(n);
+        }
 
+        //Perform the transformation operation k times
+        str = num.toString();
+        for (int i = 0; i < k; i++) {
+            int sum = 0;
+            for (char ch : str.toCharArray()) {
+                sum += ch - '0';
+            }
+            str = Integer.toString(sum);
+        }
+        
+        return Integer.parseInt(str);
+    }
+}
