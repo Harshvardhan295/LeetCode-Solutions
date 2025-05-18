92c0439e77bf91804f504e54d0785b2352b40ba0
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-05-18 22:42:34 +0530

    Update 416. Partition Equal Subset Sum.java

--- Recursion/Java-Leetcode-Solutions/416. Partition Equal Subset Sum.java
+++ Recursion/Java-Leetcode-Solutions/416. Partition Equal Subset Sum.java
@@ -50,3 +50,28 @@ class Solution {
         return subsetMemo(0,nums,c,dp);
     }
 }
+//Tabulation
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum=0,C=Integer.MIN_VALUE, n=nums.length;
+        for(int ele:nums) sum+=ele;
+        //if array sum is odd, no two equal subsets can be created
+        if(sum%2!=0) return false;
+        C=sum/2;
+
+        int[][] dp=new int[n][C+1];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<C+1;j++){
+                boolean ans = false;
+                boolean skip = (i>0) ? (dp[i-1][j]==1) : (j==0);
+                if(j-nums[i]<0) ans=skip;
+                else{
+                    boolean take = (i>0) ? (dp[i-1][j-nums[i]]==1) : (j==0);
+                    ans=take||skip;
+                }
+                dp[i][j]= (ans) ? 1:0;
+            }
+        }
+        return (dp[n-1][C]==1);
+    }
+}
