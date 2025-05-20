d5451eaf4fdcf145238974934888f7eec40eb67f
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-05-20 16:43:18 +0530

    Update 135. Candy.cpp

--- Hard_problems/C++_LeetCode_Solutions/135. Candy.cpp
+++ Hard_problems/C++_LeetCode_Solutions/135. Candy.cpp
@@ -1,18 +1,19 @@
 class Solution {
-    public int candy(int[] arr) {
-        int n=arr.length;
-        int[] numCandies=new int[n];
-        for(int i=0;i<n;i++) numCandies[i]=1;//give 1 candie to each 
+public:
+    int candy(vector<int>& arr) {
+        int n=arr.size();
+        vector<int> numCandies(n, 1);//give 1 candie to each 
+
         //Left to right pass
         for(int i=1;i<n;i++) if(arr[i]>arr[i-1]) numCandies[i] = numCandies[i-1] + 1;
         //Right to left pass
         for (int i= n-2;i>=0;i--) {
             if (arr[i]>arr[i+1]) {
-                numCandies[i] =Math.max(numCandies[i],numCandies[i+1]+1);
+                numCandies[i] =max(numCandies[i],numCandies[i+1]+1);
             }
         }
         int sum=0;
         for(int ele:numCandies) sum+=ele;
         return sum;
     }
-}
+};
