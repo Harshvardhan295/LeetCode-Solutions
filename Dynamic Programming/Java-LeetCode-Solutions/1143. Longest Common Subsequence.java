e21d0f38616c85055c9db78d622463b2c0ff9f72
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-05-26 21:47:43 +0530

    Create 1143. Longest Common Subsequence.java

---
+++ Dynamic Programming/Java-LeetCode-Solutions/1143. Longest Common Subsequence.java
@@ -0,0 +1,66 @@
+//Recursion 
+class Solution {
+    public int longestCommonSubsequence(String a, String b) {
+        int m=a.length(),n=b.length();
+        if(m==0||n==0) return 0;
+        String a2=a.substring(0,m-1);//does not includes m-1
+        String b2=b.substring(0,n-1);//does not includes n-1
+        if(a.charAt(m-1)==b.charAt(n-1)){
+            return 1+longestCommonSubsequence(a2,b2);
+        }
+        else{
+            int atake=longestCommonSubsequence(a2,b);
+            int btake=longestCommonSubsequence(a,b2);
+            return Math.max(atake,btake);
+        }
+    }
+}
+//Memoization using stringbuilder
+class Solution {
+    //Memoization
+    public int lcs(int i, int j,StringBuilder a, StringBuilder b,int[][] dp){
+        if(i<0||j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(a.charAt(i)==b.charAt(j)){
+            return dp[i][j]= 1+lcs(i-1,j-1,a,b,dp);
+        }
+        else{
+            int atake=lcs(i-1,j,a,b,dp);
+            int btake=lcs(i,j-1,a,b,dp);
+            return dp[i][j]=Math.max(atake,btake);
+        }
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        //StringBuilder is mutable, so it's slightly more memory-efficient if strings are modified often. However, in this case, no modification happens, so both perform the same.
+        StringBuilder a = new StringBuilder(text1);
+        StringBuilder b = new StringBuilder(text2);
+        int m=a.length(),n=b.length();
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++) dp[i][j]=-1;
+        return lcs(m-1,n-1,a,b,dp);
+    }
+}
+//Tabulation
+class Solution {
+    //tabulation with space optimisation
+    public int longestCommonSubsequence(String a, String b) {       
+        int m=a.length(),n=b.length();
+        int[][] dp=new int[2][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++) {
+                if(a.charAt(i)==b.charAt(j)){
+                     dp[1][j]= 1+ ((i>0 && j>0) ? dp[0][j-1] : 0);
+                }
+                else{
+                    int atake=(i>0) ? dp[0][j]:0;
+                    int btake=(j>0) ? dp[1][j-1] :0 ;
+                    dp[1][j]=Math.max(atake,btake);
+                }
+            }
+            //copy paste
+            for(int j=0;j<n;j++) dp[0][j]=dp[1][j];
+        }
+        return dp[1][n-1];
+    }
+}
