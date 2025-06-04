1b09e554791f3b0fa660e45d77b42f9ff0909362
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-06-04 21:19:39 +0530

    Update 392. Is Subsequence.java

--- Recursion/Java-Leetcode-Solutions/392. Is Subsequence.java
+++ Recursion/Java-Leetcode-Solutions/392. Is Subsequence.java
@@ -1,4 +1,4 @@
-//Recursion
+v//Recursion
 class Solution {
     public static int checkSubsequence(String s, String t,int i, int j,int[][] dp){
         if(i>=s.length())   return 1;
@@ -42,4 +42,24 @@ class Solution {
         return (ans==1);
     }
 }
-//Tabulation
+//tabulation
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        //filling table bottom-right to top-left
+        for (int i = m - 1; i >= 0; i--) {
+        for (int j = n - 1; j >= 0; j--) {
+            if (s.charAt(i) == t.charAt(j)) {
+                dp[i][j] = 1 + dp[i + 1][j + 1];
+            } else {
+                dp[i][j] = dp[i][j + 1];
+            }
+        }
+    }
+        return dp[0][0] == m;
+    }
+}
