44fbf06ab83cc9137808f61c12f48bf7d32a8b03
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-12-29 22:23:26 +0530

    Add memoization to minimum ASCII delete sum solution
    Implemented memoization to optimize the minimum ASCII delete sum calculation.

--- Medium_problems/Java-Leetcode-Solutions/712. Minimum ASCII Delete Sum for Two Strings.java
+++ Medium_problems/Java-Leetcode-Solutions/712. Minimum ASCII Delete Sum for Two Strings.java
@@ -20,3 +20,31 @@ class Solution {
         return minSteps(m-1,n-1,a,b);
     }
 }
+//Memoization
+class Solution {
+    int[][] dp;
+
+    public int helper(String s1, String s2, int i, int j, int m, int n) {
+        if (i == m && j == n) return 0;
+        if (i == m) return s2.charAt(j) + helper(s1, s2, i, j+1, m, n);
+        if (j == n) return s1.charAt(i) + helper(s1, s2, i+1, j, m, n);
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return dp[i][j] = helper(s1, s2, i+1, j+1, m, n);
+        }
+
+        int del1 = s1.charAt(i) + helper(s1, s2, i+1, j, m, n);
+        int del2 = s2.charAt(j) + helper(s1, s2, i, j+1, m, n);
+
+        return dp[i][j] = Math.min(del1, del2);
+    }
+
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        dp = new int[m][n];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        return helper(s1, s2, 0, 0, m, n);
+    }
+}
