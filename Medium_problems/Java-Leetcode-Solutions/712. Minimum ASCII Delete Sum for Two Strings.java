bbfdb6df0aa7181118b463728ab6f8d649afbe41
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-12-29 22:21:51 +0530

    Implement solution for Minimum ASCII Delete Sum problem

---
+++ Medium_problems/Java-Leetcode-Solutions/712. Minimum ASCII Delete Sum for Two Strings.java
@@ -0,0 +1,22 @@
+//Recursion
+class Solution {
+    public int minSteps(int i,int j, StringBuilder a, StringBuilder b){
+        if(i==-1) return j+1;
+        if(j==-1) return i+1;
+        if(a.charAt(i)==b.charAt(j)){
+            return minSteps(i-1,j-1,a,b);
+        }
+        else{
+            int del=minSteps(i-1,j,a,b);
+            int ins=minSteps(i,j-1,a,b);
+            int rep=minSteps(i-1,j-1,a,b);
+            return 1+Math.min(del,Math.min(ins,rep));
+        }
+    }
+    public int minimumDeleteSum(String word1, String word2) {
+        StringBuilder a=new StringBuilder(word1);
+        StringBuilder b=new StringBuilder(word2);
+        int m=a.length(),n=b.length();
+        return minSteps(m-1,n-1,a,b);
+    }
+}
