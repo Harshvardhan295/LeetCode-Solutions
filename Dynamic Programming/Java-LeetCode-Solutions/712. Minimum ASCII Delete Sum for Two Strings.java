11dc9da02c32cef0d9348d2faea72bb8cc31e2d7
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2025-12-29 22:24:59 +0530

    Implement Minimum ASCII Delete Sum with three methods

---
+++ Dynamic Programming/Java-LeetCode-Solutions/712. Minimum ASCII Delete Sum for Two Strings.java
@@ -0,0 +1,84 @@
+//Recursion
+class Solution {
+    public int minSteps(int i,int j, StringBuilder a, StringBuilder b){
+        if(i==-1) return j+1;
+        if(j==-1) return i+1;
+        if(a.charAt(i)==b.charAt(j)){
+            return minSteps(i-1,j-1,a,b);
+        }
+        else{
+            int del=minSteps(i-1,j,a,b);
+            int ins=minSteps(i,j-1,a,b);
+            int rep=minSteps(i-1,j-1,a,b);
+            return 1+Math.min(del,Math.min(ins,rep));
+        }
+    }
+    public int minimumDeleteSum(String word1, String word2) {
+        StringBuilder a=new StringBuilder(word1);
+        StringBuilder b=new StringBuilder(word2);
+        int m=a.length(),n=b.length();
+        return minSteps(m-1,n-1,a,b);
+    }
+}
+//Memoization
+class Solution {
+    int[][] dp;
+
+    public int helper(String s1, String s2, int i, int j, int m, int n) {
+        if (i == m && j == n) return 0;
+        if (i == m) return s2.charAt(j) + helper(s1, s2, i, j+1, m, n);
+        if (j == n) return s1.charAt(i) + helper(s1, s2, i+1, j, m, n);
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return dp[i][j] = helper(s1, s2, i+1, j+1, m, n);
+        }
+
+        int del1 = s1.charAt(i) + helper(s1, s2, i+1, j, m, n);
+        int del2 = s2.charAt(j) + helper(s1, s2, i, j+1, m, n);
+
+        return dp[i][j] = Math.min(del1, del2);
+    }
+
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        dp = new int[m][n];
+        for (int[] row : dp) Arrays.fill(row, -1);
+        return helper(s1, s2, 0, 0, m, n);
+    }
+}
+
+//Tabulation
+class Solution {
+    public int minimumDeleteSum(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        // filling last row
+        for (int i = n - 1; i >= 0; i--) {
+            dp[m][i] = s2.charAt(i) + dp[m][i + 1];
+        }
+
+        // filling last column
+        for (int i = m - 1; i >= 0; i--) {
+            dp[i][n] = s1.charAt(i) + dp[i + 1][n];
+        }
+
+        // Filling table bottom-up
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.min(
+                            s1.charAt(i) + dp[i + 1][j],
+                            s2.charAt(j) + dp[i][j + 1]
+                    );
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
