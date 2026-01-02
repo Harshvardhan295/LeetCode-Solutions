38eb8b4dc35970f937ca911299b3ae83b05c3526
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-01-02 23:35:07 +0530

    Update 15. 3Sum.java

--- Medium_problems/Java-Leetcode-Solutions/15. 3Sum.java
+++ Medium_problems/Java-Leetcode-Solutions/15. 3Sum.java
@@ -1,1 +1,65 @@
+//Optimised approach
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> listOfLists = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return listOfLists; 
+        }
+        Arrays.sort(nums);
 
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) { //skip duplicates
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    listOfLists.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return listOfLists;
+    }
+}
+//Brute Force
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                        triplet.sort(Integer::compareTo);
+                        
+                        if (!result.contains(triplet)) {
+                            result.add(triplet);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+}}
