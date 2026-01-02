483c9864b318c0512a5255622715b265a6acf489
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-01-02 23:33:23 +0530

    Update 1470. Shuffle the Array.java

--- Easy_problems/Java-Leetcode-Solutions/1470. Shuffle the Array.java
+++ Easy_problems/Java-Leetcode-Solutions/1470. Shuffle the Array.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans=new int[2*n];
+        int j=0,k=1;
+        for(int i=0;i<2*n;i+=2){
+            ans[i]=nums[j];
+            ans[k]=nums[j+n];
+            j++;
+            k+=2;
+        }
+        return ans;
+    }
+}
