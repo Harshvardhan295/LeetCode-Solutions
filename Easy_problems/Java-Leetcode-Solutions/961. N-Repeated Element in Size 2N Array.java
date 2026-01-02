cf5753ad0b3ece29000c2b9ecc883574cc6c84a4
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-01-02 17:21:02 +0530

    Implement N-repeated element solution with HashMap
    Added an alternative implementation using HashMap to find the N-repeated element.

--- Easy_problems/Java-Leetcode-Solutions/961. N-Repeated Element in Size 2N Array.java
+++ Easy_problems/Java-Leetcode-Solutions/961. N-Repeated Element in Size 2N Array.java
@@ -1,3 +1,4 @@
+//HashSet
 class Solution {
     public int repeatedNTimes(int[] nums) {
         Set<Integer> s = new HashSet<>();
@@ -7,5 +8,23 @@ class Solution {
               return i;
         }
         return nums[nums.length-1];
+    }
+}
+//HashMap
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        }
+        int maxCnt=Integer.MIN_VALUE;
+        int maxNum=-1;
+        for(int key:mp.keySet()){
+            if(mp.get(key)>maxCnt) {
+                maxCnt=mp.get(key);
+                maxNum=key;
+            }
+        }
+        return maxNum;
     }
 }
