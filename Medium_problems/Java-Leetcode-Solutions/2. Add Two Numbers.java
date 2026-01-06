e868d7fd5d21027abdf41204c5a5a02d001b5517
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-01-06 23:51:45 +0530

    Refactor addTwoNumbers for better performance
    Refactored addTwoNumbers method for improved readability and efficiency.

--- Medium_problems/Java-Leetcode-Solutions/2. Add Two Numbers.java
+++ Medium_problems/Java-Leetcode-Solutions/2. Add Two Numbers.java
@@ -40,3 +40,25 @@ if (carry==1){
 return dummy.next;
     }
 }
+// Shorter code
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode ans = new ListNode();
+        ListNode temp = ans;
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+
+            temp.next = new ListNode(sum % 10);
+            temp = temp.next;
+
+            if(l1!=null) l1 = l1.next;
+            if(l2!=null ) l2 = l2.next;
+        }
+        return ans.next;
+    }
+}
