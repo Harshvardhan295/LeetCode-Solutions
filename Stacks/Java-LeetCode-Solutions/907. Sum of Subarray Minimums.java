8267b6d2b0ef22bf992dea094ea04cf7973615a4
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-01-14 19:08:30 +0530

    Create 907. Sum of Subarray Minimums.java

---
+++ Stacks/Java-LeetCode-Solutions/907. Sum of Subarray Minimums.java
@@ -0,0 +1,70 @@
+//dp with TLE
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int MOD = 1000000007;
+        int[][] dp = new int[n][n];
+        long sum = 0;
+
+        // base case
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = arr[i];
+            sum = (sum + arr[i]) % MOD;
+        }
+
+        // fill dp table
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i][j - 1], arr[j]);
+                sum = (sum + dp[i][j]) % MOD;
+            }
+        }
+
+        return (int) sum;
+    }
+}
+//stack
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int length = arr.length;
+        int[] left = new int[length];
+        int[] right = new int[length];
+      
+        Arrays.fill(left, -1);
+        Arrays.fill(right, length);
+      
+        Deque<Integer> stack = new ArrayDeque<>();
+      
+        for (int i = 0; i < length; ++i) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                left[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+      
+        stack.clear();
+      
+        for (int i = length - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                right[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+      
+        int mod = (int) 1e9 + 7;
+        long answer = 0;
+      
+        for (int i = 0; i < length; ++i) {
+            answer += (long) (i - left[i]) * (right[i] - i) % mod * arr[i] % mod;
+            answer %= mod;
+        }
+      
+        return (int) answer;
+    }
+}
