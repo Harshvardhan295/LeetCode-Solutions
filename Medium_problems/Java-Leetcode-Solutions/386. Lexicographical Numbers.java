0e0c651c7a372a20bf90bbb6934ca54a1abd13bd
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-01-21 11:39:49 +0530

    Update 386. Lexicographical Numbers.java

--- Medium_problems/Java-Leetcode-Solutions/386. Lexicographical Numbers.java
+++ Medium_problems/Java-Leetcode-Solutions/386. Lexicographical Numbers.java
@@ -1,3 +1,4 @@
+//method 1
 class Solution {
     public List<Integer> lexicalOrder(int n) {
         List<String> l=new ArrayList<>();
@@ -11,5 +12,26 @@ class Solution {
                                 .map(Integer::parseInt) // or Integer::valueOf
                               .collect(Collectors.toList());
         return intList;
+    }
+}
+//method 2
+import java.util.*;
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> result = new ArrayList<>();
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            result.add(count);
+            if (count * 10 <= n) {
+                count *= 10;
+            } else {
+                if (count >= n) count /= 10;
+                count++;
+                while (count % 10 == 0) {
+                    count /= 10;
+                }
+            }
+        }
+        return result;
     }
 }
