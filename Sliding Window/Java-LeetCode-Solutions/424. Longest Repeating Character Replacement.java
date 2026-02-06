4b9881005ae6fd254888c455682f64d719e168f7
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-02-06 15:08:28 +0530

    Create 424. Longest Repeating Character Replacement.java

---
+++ Sliding Window/Java-LeetCode-Solutions/424. Longest Repeating Character Replacement.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int left = 0, maxFreq = 0, result = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            int idx = s.charAt(right) - 'A';
+            freq[idx]++;
+            maxFreq = Math.max(maxFreq, freq[idx]);
+
+            while ((right - left + 1) > maxFreq + k) {
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+
+            result = Math.max(result, right - left + 1);
+        }
+
+        return result;
+    }
+}
