1974e882a9306976ca348ba0f5eaa8ecc6b56377
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-02-21 23:42:07 +0530

    Add solution for maximum 69 number problem
    Implemented two methods to convert the number 6 to 9 in an integer.

---
+++ Easy_problems/Java-Leetcode-Solutions/1323. Maximum 69 Number.java
@@ -0,0 +1,35 @@
+//___________________________Method 1_______________________
+class Solution {
+	public int maximum69Number (int num) {
+		String str = Integer.toString(num);
+		String ans="";
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)=='6')
+			{
+				ans+='9';
+				break;
+			}
+			ans+=str.charAt(i);
+		}
+		if(str.length()!=ans.length())
+		{
+			ans+=str.substring(ans.length());
+		}
+		return Integer.parseInt(ans);
+	}
+}
+//_________________________Method 2______________________________
+class Solution {
+    public int maximum69Number (int num) {
+        String str = Integer.toString(num);
+        StringBuilder s = new StringBuilder(str);
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '6'){
+                s.setCharAt(i,'9');
+                break;
+            }
+        }
+        return Integer.parseInt(s.toString());
+    }
+}
