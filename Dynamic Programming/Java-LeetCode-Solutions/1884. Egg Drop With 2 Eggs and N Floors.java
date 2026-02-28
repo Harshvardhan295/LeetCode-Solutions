2b506813d5b7486a3b194935b7cf712b9a2fad97
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-02-28 23:55:30 +0530

    Update 1884. Egg Drop With 2 Eggs and N Floors.java

--- Dynamic Programming/Java-LeetCode-Solutions/1884. Egg Drop With 2 Eggs and N Floors.java
+++ Dynamic Programming/Java-LeetCode-Solutions/1884. Egg Drop With 2 Eggs and N Floors.java
@@ -1,1 +1,31 @@
+//DP with memoization
+class Solution {
+    int[][] dp = new int[1001][3];
+    
+    int ok(int n, int eggs) {
+        if (dp[n][eggs] != -1) return dp[n][eggs];
 
+        if (n <= 1) return n;
+        if (eggs == 1) return n;
+
+        int mini = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= n; i++) {
+            int breakCase = ok(i - 1, eggs - 1);
+            int surviveCase = ok(n - i, eggs);
+            int worstMoves = 1 + Math.max(breakCase, surviveCase);
+            mini = Math.min(mini, worstMoves);
+        }
+
+        return dp[n][eggs] = mini;
+    }
+
+    public int twoEggDrop(int n) {
+        for (int i = 0; i < 1001; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return ok(n, 2);
+    }
+}
