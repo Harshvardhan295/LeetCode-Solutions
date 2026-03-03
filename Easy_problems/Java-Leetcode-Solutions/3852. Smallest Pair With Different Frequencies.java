cf51605725ebad0d0cce6f4579875e7efc9d9f07
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-03 22:03:14 +0530

    Add solution for smallest pair with different frequencies
    Implement minDistinctFreqPair method to find smallest pair with different frequencies.

--- Easy_problems/Java-Leetcode-Solutions/3852. Smallest Pair With Different Frequencies.java
+++ Easy_problems/Java-Leetcode-Solutions/3852. Smallest Pair With Different Frequencies.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int[] minDistinctFreqPair(int[] nums) {
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        int n=nums.length;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+            min=Math.min(min,nums[i]);
+        }
+        int secmin=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=min && mp.get(nums[i])!=mp.get(min) ) {
+                secmin=Math.min(secmin,nums[i]);
+            }
+        }
+        if(secmin==Integer.MAX_VALUE) return new int[]{-1,-1};
+        return new int[]{min,secmin};
+        
+    }
+}
