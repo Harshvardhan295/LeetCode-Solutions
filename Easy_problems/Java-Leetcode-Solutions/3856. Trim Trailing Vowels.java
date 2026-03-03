b1fefcd637b94e78f37042109944164a062ab811
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-03 22:04:32 +0530

    Implement method to trim trailing vowels from string

--- Easy_problems/Java-Leetcode-Solutions/3856. Trim Trailing Vowels.java
+++ Easy_problems/Java-Leetcode-Solutions/3856. Trim Trailing Vowels.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public String trimTrailingVowels(String s) {
+        StringBuilder str=new StringBuilder(s);
+        int i=s.length()-1;
+        while(i>=0){
+            if("aeiou".indexOf(str.charAt(i))!=-1) {
+                str.deleteCharAt(i--);
+            }
+            else break;
+        }
+        return str.toString();
+    }
+}
