5333344e91ca272b160faddc96adfae46f9d0fee
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-03 22:06:37 +0530

    Update 3857. Minimum Cost to Split into Ones.java

--- Medium_problems/Java-Leetcode-Solutions/3857. Minimum Cost to Split into Ones.java
+++ Medium_problems/Java-Leetcode-Solutions/3857. Minimum Cost to Split into Ones.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    int ans=0;
+    public int helper(int n){
+        if(n==1) return 0;
+        int a=n/2;
+        int b=n-a;
+        ans+=a*b;
+        return helper(a)+helper(b);
+    }
+    public int minCost(int n) {
+        helper(n);
+        return ans;        
+    }
+}
