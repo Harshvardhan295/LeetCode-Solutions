b256bcbb0662e88b958ee556b6e99f37bb4257bc
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-03 22:01:26 +0530

    Add distinctDifferenceArray method in Solution class
    Implement distinctDifferenceArray method to calculate the difference between distinct counts in prefix and suffix.

--- Hash Table/HashSet/Java-LeetCode-Solutions/2670. Find the Distinct Difference Array.java
+++ Hash Table/HashSet/Java-LeetCode-Solutions/2670. Find the Distinct Difference Array.java
@@ -1,1 +1,29 @@
+class Solution {
+    public int[] distinctDifferenceArray(int[] nums) {
+        int n = nums.length;
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+        Set<Integer> prefixSet = new HashSet<>();
+        Set<Integer> suffixSet = new HashSet<>();
 
+        prefix[0] = 1;
+        prefixSet.add(nums[0]);
+        
+
+        for(int i = 1;i<n;i++){
+            prefixSet.add(nums[i]);
+            prefix[i] = prefixSet.size();
+        }
+        for(int i = n-1;i>=0;i--){
+            suffix[i] = suffixSet.size();
+            suffixSet.add(nums[i]);
+        }
+
+        int ans[] = new int[n];
+        for(int i = 0;i<n;i++){
+            ans[i] = prefix[i] - suffix[i];
+        }
+        return ans;
+
+    }
+}
