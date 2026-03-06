8ca411f9ac35d3f1770077fc7376035befe433e2
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-06 20:55:41 +0530

    Update 122. Best Time to Buy and Sell Stock II.java

--- Medium_problems/Java-Leetcode-Solutions/122. Best Time to Buy and Sell Stock II.java
+++ Medium_problems/Java-Leetcode-Solutions/122. Best Time to Buy and Sell Stock II.java
@@ -1,1 +1,23 @@
+//Recursion
+// TC = 2^n
+// SC= n
+class Solution {
+    int profit = 0;
 
+    public int rec(int[] prices, int idx, int buy) {
+        if (idx == prices.length)
+            return 0;
+        if (buy == 1) {
+            profit = Math.max(-prices[idx] + rec(prices, idx + 1, 0),
+                    0 + rec(prices, idx + 1, 1));
+        } else {
+            profit = Math.max(prices[idx] + rec(prices, idx + 1, 1),
+                    0 + rec(prices, idx + 1, 0));
+        }
+        return profit;
+    }
+
+    public int maxProfit(int[] prices) {
+        return rec(prices, 0, 1);
+    }
+}
