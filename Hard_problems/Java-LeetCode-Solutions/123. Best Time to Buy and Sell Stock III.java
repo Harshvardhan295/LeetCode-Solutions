0e75767070e952c0a1d28f884accd15d70c618f2
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-09 23:48:54 +0530

    Update 123. Best Time to Buy and Sell Stock III.java

--- Hard_problems/Java-LeetCode-Solutions/123. Best Time to Buy and Sell Stock III.java
+++ Hard_problems/Java-LeetCode-Solutions/123. Best Time to Buy and Sell Stock III.java
@@ -38,3 +38,43 @@ class Solution {
         return rec(prices, 0, 1, 2);
     }
 }
+// Memoization
+// more auxillary space..
+class Solution {
+
+    public int rec(int[] prices, int idx, int buy, int cap, int[][][] dp) {
+
+        if (idx == prices.length || cap == 0)
+            return 0;
+
+        if (dp[idx][buy][cap] != -1)
+            return dp[idx][buy][cap];
+
+        int profit;
+
+        if (buy == 1) {
+            profit = Math.max(
+                    -prices[idx] + rec(prices, idx + 1, 0, cap, dp),
+                    rec(prices, idx + 1, 1, cap, dp));
+        } else {
+            profit = Math.max(
+                    prices[idx] + rec(prices, idx + 1, 1, cap - 1, dp),
+                    rec(prices, idx + 1, 0, cap, dp));
+        }
+
+        return dp[idx][buy][cap] = profit;
+    }
+
+    public int maxProfit(int[] prices) {
+
+        int n = prices.length;
+        int[][][] dp = new int[n][2][3];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 3; k++)
+                    dp[i][j][k] = -1;
+
+        return rec(prices, 0, 1, 2, dp);
+    }
+}
