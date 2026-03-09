ccecb82481270aa608dd6d2919eac4c91b4df58c
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-09 23:50:54 +0530

    Create 123. Best Time to Buy and Sell Stock III.java

---
+++ Dynamic Programming/Java-LeetCode-Solutions/123. Best Time to Buy and Sell Stock III.java
@@ -0,0 +1,144 @@
+// Basic logic
+class Solution {
+    public int maxProfit(int[] prices) {
+        // at max - 2 transactions
+        // firstbuy -> first sell -> second buy -> second sell
+        int fb = Integer.MIN_VALUE, fs = 0;
+        int sb = Integer.MIN_VALUE, ss = 0;
+        for (int i = 0; i < prices.length; i++) {
+            fb = Math.max(fb, -prices[i]);
+            fs = Math.max(fs, fb + prices[i]);
+            sb = Math.max(sb, fs - prices[i]);
+            ss = Math.max(ss, sb + prices[i]);
+        }
+        return ss;
+    }
+}
+//Recursion
+// TC = 2^n
+// SC= n
+class Solution {
+
+    public int rec(int[] prices, int idx, int buy, int cap) {
+        if (cap == 0 || idx == prices.length)
+            return 0;
+
+        int profit;
+        if (buy == 1) {
+            profit = Math.max(-prices[idx] + rec(prices, idx + 1, 0, cap),
+                    0 + rec(prices, idx + 1, 1, cap));
+        } else {
+            profit = Math.max(prices[idx] + rec(prices, idx + 1, 1, cap - 1),
+                    0 + rec(prices, idx + 1, 0, cap));
+        }
+        return profit;
+    }
+
+    public int maxProfit(int[] prices) {
+        return rec(prices, 0, 1, 2);
+    }
+}
+// Memoization
+// more auxillary space..
+class Solution {
+
+    public int rec(int[] prices, int idx, int buy, int cap, int[][][] dp) {
+
+        if (idx == prices.length || cap == 0)
+            return 0;
+
+        if (dp[idx][buy][cap] != -1)
+            return dp[idx][buy][cap];
+
+        int profit;
+
+        if (buy == 1) {
+            profit = Math.max(
+                    -prices[idx] + rec(prices, idx + 1, 0, cap, dp),
+                    rec(prices, idx + 1, 1, cap, dp));
+        } else {
+            profit = Math.max(
+                    prices[idx] + rec(prices, idx + 1, 1, cap - 1, dp),
+                    rec(prices, idx + 1, 0, cap, dp));
+        }
+
+        return dp[idx][buy][cap] = profit;
+    }
+
+    public int maxProfit(int[] prices) {
+
+        int n = prices.length;
+        int[][][] dp = new int[n][2][3];
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < 2; j++)
+                for (int k = 0; k < 3; k++)
+                    dp[i][j][k] = -1;
+
+        return rec(prices, 0, 1, 2, dp);
+    }
+}
+//Tabulation
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int[][][] dp = new int[n + 1][2][3];
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+
+                    if (buy == 1) {
+                        dp[idx][buy][cap] = Math.max(
+                                -prices[idx] + dp[idx + 1][0][cap],
+                                dp[idx + 1][1][cap] );
+                    } else {
+                        dp[idx][buy][cap] = Math.max(
+                                prices[idx] + dp[idx + 1][1][cap - 1],
+                                dp[idx + 1][0][cap] );
+                    }
+
+                }
+            }
+        }
+
+        return dp[0][1][2];
+    }
+}
+
+//Space Optimization
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] ahead = new int[2][3];
+        int[][] cur = new int[2][3];
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int cap = 1; cap <= 2; cap++) {
+
+                    if (buy == 1) {
+                        cur[buy][cap] = Math.max(
+                                -prices[ind] + ahead[0][cap],
+                                ahead[1][cap] );
+                    } 
+                    else {
+                        cur[buy][cap] = Math.max(
+                                prices[ind] + ahead[1][cap - 1],
+                                ahead[0][cap] );
+                    }
+                }
+            }
+            //problem with java can't directly put value unless it is not 1D
+            // copy current → ahead
+            for (int i = 0; i < 2; i++) { 
+                for (int j = 0; j < 3; j++) {
+                    ahead[i][j] = cur[i][j];
+                }
+            }
+        }
+
+        return ahead[1][2];
+    }
+}
