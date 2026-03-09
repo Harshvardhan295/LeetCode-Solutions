c028918d10242bb9431fac74e61c643f56068d57
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-09 23:55:04 +0530

    Update 188. Best Time to Buy and Sell Stock IV.java

--- Dynamic Programming/Java-LeetCode-Solutions/188. Best Time to Buy and Sell Stock IV.java
+++ Dynamic Programming/Java-LeetCode-Solutions/188. Best Time to Buy and Sell Stock IV.java
@@ -1,1 +1,23 @@
+//Recursion
+// TC = 2^n
+// SC= n
+class Solution {
+    public int rec(int[] prices, int idx, int buy, int cap) {
+        if (cap == 0 || idx == prices.length)
+            return 0;
 
+        int profit;
+        if (buy == 1) {
+            profit = Math.max(-prices[idx] + rec(prices, idx + 1, 0, cap),
+                    0 + rec(prices, idx + 1, 1, cap));
+        } else {
+            profit = Math.max(prices[idx] + rec(prices, idx + 1, 1, cap - 1),
+                    0 + rec(prices, idx + 1, 0, cap));
+        }
+        return profit;
+    }
+
+    public int maxProfit(int k, int[] prices) {
+        return rec(prices, 0, 1, k);
+    }
+}
