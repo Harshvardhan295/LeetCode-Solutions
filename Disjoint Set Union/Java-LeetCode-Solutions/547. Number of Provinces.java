3ced5595d8a9896b7268e1929d7728c83281be92
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-03-24 23:55:50 +0530

    Update 547. Number of Provinces.java

--- Disjoint Set Union/Java-LeetCode-Solutions/547. Number of Provinces.java
+++ Disjoint Set Union/Java-LeetCode-Solutions/547. Number of Provinces.java
@@ -1,1 +1,123 @@
+// ---------------Optimization using union by size + Path compression----------------------
+class Solution {
+    //Optimization - Balanced trees
+    static int[] parent;
+    static int[] size;
+    public int find(int a){
+        if(parent[a]==a) return a;
+        return parent[a]=find(parent[a]);
+    }
+    public void union(int a, int b){
+        a=find(a);
+        b=find(b);
+        if(a!=b){
+            if(size[a]>size[b]){ // attach smaller set to bigger set
+                parent[b]=a; //make a parent of b
+                size[a]+=size[b]; //update size of bigger set
+            } else{
+                parent[a]=b; //make b parent of a
+                size[b]+=size[a]; //update size of bigger set
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        parent=new int[n+1]; // 1 to n nodes
+        size=new int[n+1];
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j]==1) union(i+1, j+1);
+            }
+        }
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(parent[i]==i) count++;
+        }
+        return count;
+    }
+}
 
+// --------------------Optimization using union by size---------------------------------
+class Solution {
+    static int[] parent;
+    static int[] size;
+    public int find(int a){
+        if(parent[a]==a) return a;
+        return find(parent[a]);
+    }
+    public void union(int a, int b){
+        a=find(a);
+        b=find(b);
+        if(a!=b){
+            if(size[a]>size[b]){ // attach smaller set to bigger set
+                parent[b]=a; //make a parent of b
+                size[a]+=size[b]; //update size of bigger set
+            } else{
+                parent[a]=b; //make b parent of a
+                size[b]+=size[a]; //update size of bigger set
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        parent=new int[n+1]; // 1 to n nodes
+        size=new int[n+1];
+        for(int i=1;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j]==1) union(i+1, j+1);
+            }
+        }
+        int count=0;
+        for(int i=1;i<=n;i++){
+            if(parent[i]==i) count++;
+        }
+        return count;
+    }
+}
+
+//---------------------- Without optimization O(n^2)------------------------
+class Solution {
+    static int[] parent;
+    public int find(int a) {
+        if (parent[a] == a)
+            return a;
+        return find(parent[a]);
+    }
+
+    public void union(int a, int b) {
+        int leaderA = find(a);
+        int leaderB = find(b);
+        if (leaderA != leaderB) {
+            parent[leaderB] = leaderA;
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        parent = new int[n + 1]; // 1 to n nodes
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && isConnected[i][j] == 1)
+                    union(i + 1, j + 1);
+            }
+        }
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (parent[i] == i)
+                count++;
+        }
+        return count;
+    }
+
+}
