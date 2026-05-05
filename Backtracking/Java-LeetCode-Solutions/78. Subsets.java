1c8ddc961269739d27e281803984d10886e549f7
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-05-05 23:52:05 +0530

    Update 78. Subsets.java

--- Backtracking/Java-LeetCode-Solutions/78. Subsets.java
+++ Backtracking/Java-LeetCode-Solutions/78. Subsets.java
@@ -1,3 +1,4 @@
+//Method 1 - Bit Manipulation
 class Solution {
     public List<List<Integer>> subsets(int[] arr) {
         int n = arr.length;
@@ -14,5 +15,25 @@ class Solution {
             ans.add(l);
         }
         return ans;
+    }
+}
+//Method 2 - Backtracking
+class Solution {
+        static List<List<Integer>> arraylist = new ArrayList<>(); // global
+
+    public static void printsubsetarray(int i, int[] arr, ArrayList<Integer>currentsubset) {
+        if (i == arr.length) {
+            arraylist.add(new ArrayList<Integer>(currentsubset));
+            return;
+        }
+        printsubsetarray(i + 1, arr, currentsubset); // Not take
+        currentsubset.add(arr[i]);
+        printsubsetarray(i + 1, arr, currentsubset); // Take
+        currentsubset.remove(currentsubset.size() - 1);
+    }
+    public List<List<Integer>> subsets(int[] arr) {
+        arraylist=new ArrayList<>();
+       printsubsetarray(0,arr,new ArrayList<>());
+        return arraylist;
     }
 }
