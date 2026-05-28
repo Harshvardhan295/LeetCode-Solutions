439b9ffa13c960d8211b461c656c9d2c3250338d
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-05-28 23:37:39 +0530

    Add solution for first matching character from both ends

---
+++ Easy_problems/Java-Leetcode-Solutions/3884. First Matching Character From Both Ends.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int i=0,j=s.length()-i-1;
+        while(i<=j){
+            if(s.charAt(i)==s.charAt(j)) return i;
+            i++;
+            j--;
+        }
+        return -1;
+    }
+}
