453f8f995bc81d7ab22839b9472112c855ad5b98
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-06-13 23:43:18 +0530

    Update 706. Design HashMap.java

--- Easy_problems/Java-Leetcode-Solutions/706. Design HashMap.java
+++ Easy_problems/Java-Leetcode-Solutions/706. Design HashMap.java
@@ -15,3 +15,81 @@ class MyHashMap {
         data[key] = -1;
     }
 }
+
+// Method 2
+class Pair {
+    int key;
+    int val;
+    Pair next;
+
+    Pair(int key, int val) {
+        this.key = key;
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class MyHashMap {
+    private int hash(int key) {
+        return key % 1000;
+    }
+
+    private Pair[] map;
+
+    public MyHashMap() {
+        map = new Pair[1000];
+        for (int i = 0; i < 1000; i++) {
+            map[i] = new Pair(-1, -1);
+        }
+    }
+
+    public void put(int key, int value) {
+        int hash = hash(key);
+        Pair cur = map[hash];
+
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next.val = value;
+                return;
+            }
+            cur = cur.next;
+        }
+
+        cur.next = new Pair(key, value);
+    }
+
+    public int get(int key) {
+        int hash = hash(key);
+        Pair cur = map[hash].next;
+
+        while (cur != null) {
+            if (cur.key == key)
+                return cur.val;
+            cur = cur.next;
+        }
+
+        return -1;
+    }
+
+    public void remove(int key) {
+        int hash = hash(key);
+        Pair cur = map[hash];
+
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next = cur.next.next;
+                return;
+            }
+            cur = cur.next;
+        }
+    }
+
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
