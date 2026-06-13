21aaaf0a20fd33788ef751437640770a1a551f3d
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-06-14 00:02:19 +0530

    Create 567. Permutation in String.java

---
+++ Medium_problems/Java-Leetcode-Solutions/567. Permutation in String.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+
+        if(n > m) return false;
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        // frequency of s1
+        for(int i = 0; i < n; i++){
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        // first window check
+        if(Arrays.equals(freq1, freq2))
+            return true;
+
+        // sliding window
+        for(int i = n; i < m; i++){
+
+            freq2[s2.charAt(i) - 'a']++;
+
+            freq2[s2.charAt(i - n) - 'a']--;
+
+            if(Arrays.equals(freq1, freq2))
+                return true;
+        }
+
+        return false;
+    }
+}
