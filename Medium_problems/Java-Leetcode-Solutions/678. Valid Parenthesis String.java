ca15b1dd5a36ed9b6cfb5710556c261a12e0e63d
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-06-16 21:58:02 +0530

    Create 678. Valid Parenthesis String.java

---
+++ Medium_problems/Java-Leetcode-Solutions/678. Valid Parenthesis String.java
@@ -0,0 +1,55 @@
+// simple optimised
+class Solution {
+    public boolean checkValidString(String s) {
+        int minBrackets = 0;
+        int maxBrackets = 0;
+
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                minBrackets++;
+                maxBrackets++;
+            }else if(ch == '*'){
+                minBrackets = Math.max(minBrackets - 1, 0);
+                maxBrackets++;
+            }else{
+                minBrackets = Math.max(minBrackets - 1, 0);
+                maxBrackets--;
+            }
+
+            if(maxBrackets < 0) return false;
+        }
+
+        if(minBrackets == 0) return true;
+
+        return false;
+    }
+}
+// Stack solution
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer>  bracketsStack = new Stack<>();
+        Stack<Integer> asteriskStack = new Stack<>();
+        char [] sArr = s.toCharArray(); 
+        for(int i=0;i<sArr.length;i++){
+            char ch = sArr[i];
+
+            if(ch == '(')
+                bracketsStack.push(i);
+            else if(ch=='*') 
+                asteriskStack.push(i);
+            else if(!bracketsStack.isEmpty())
+                bracketsStack.pop();
+            else if(!asteriskStack.isEmpty())
+                asteriskStack.pop();
+            else
+                return false;
+        }
+
+        while(!bracketsStack.isEmpty() && !asteriskStack.isEmpty() && bracketsStack.peek()<asteriskStack.peek()){
+            bracketsStack.pop();
+            asteriskStack.pop();
+        }
+
+        return bracketsStack.isEmpty();
+    }
+}
