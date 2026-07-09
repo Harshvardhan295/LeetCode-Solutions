7ebe5f57f284b95cd568b52d88a2bcc4cd033bb2
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-07-10 00:24:40 +0530

    Create 383. Ransom Note.java

---
+++ Easy_problems/Java-Leetcode-Solutions/383. Ransom Note.java
@@ -0,0 +1,41 @@
+// Simple
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for (char c : ransomNote.toCharArray()) {
+            mp.put(c, mp.getOrDefault(c, 0) + 1);
+        }
+        HashMap<Character, Integer> mp2 = new HashMap<>();
+        for (char c : magazine.toCharArray()) {
+            mp2.put(c, mp2.getOrDefault(c, 0) + 1);
+        }
+        if (mp.size() > mp2.size())
+            return false;
+        for (char c : ransomNote.toCharArray()) {
+            if(!mp2.containsKey(c)) return false;
+            if ( mp.get(c) > mp2.get(c))
+                return false;
+        }
+        return true;
+    }
+}
+// Optimised
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int[] freq = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (freq[c - 'a'] == 0) {
+                return false;
+            }
+            freq[c - 'a']--;
+        }
+
+        return true;
+    }
+}
