e66fc01a2304bf39990d892d884959c5038427fc
Harsh Vardhan Khajuria <159396917+Harshvardhan295@users.noreply.github.com>
2026-07-31 21:19:35 +0530

    Refactor symmetric tree check with invert tree approach
    Refactored isSymmetric method and added invertTree and isSameTree methods for symmetry check.

--- Easy_problems/Java-Leetcode-Solutions/101. Symmetric Tree.java
+++ Easy_problems/Java-Leetcode-Solutions/101. Symmetric Tree.java
@@ -1,18 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+//DFS
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) return true;
@@ -30,5 +16,41 @@ class Solution {
         return (t1.val == t2.val)
             && isMirror(t1.left, t2.right)
             && isMirror(t1.right, t2.left);
+    }
+}
+// Using Invert Tree
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        if(root.left==null && root.right==null) return root;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        // if(p!=null && q==null) return false;
+        // if(p==null && q!=null) return false;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+        if (!isSameTree(p.left, q.left))
+            return false;
+        if (!isSameTree(p.right, q.right))
+            return false;
+        return true;
+    }
+    public boolean isSymmetric(TreeNode root) {
+        
+        invertTree(root.right);
+        boolean result=isSameTree(root.left,root.right);
+        invertTree(root.right);
+
+        return result;
     }
 }
